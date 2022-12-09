ec9a2963827849bf6dd1ad7651e04212eafd4cc9
kerlos1256 <kero24862@gmail.con>
2022-12-09 05:51:46 +0200

    admin seller social info

--- apps/front-end/admin/pages/sellers/edit/[id].tsx
+++ apps/front-end/admin/pages/sellers/edit/[id].tsx
@@ -7,7 +7,6 @@ import {
   SimpleTabItemList,
   SimpleTabHead,
   ProductDetailsTable,
-  HotelDetailedSearchCard,
   AffiliationHistorySection,
   AffiliationListSection,
   Stack,
@@ -16,9 +15,20 @@ import {
   HotelsSearchList,
   OrdersSection,
   Pagination,
+  SocialProfile,
+  Table,
+  TBody,
+  THead,
+  Tr,
+  Td,
+  Th,
+  Image,
+  TrashIcon,
 } from "ui";
-import { randomNum } from "utils";
+import { mapArray, randomNum } from "utils";
 import { lngs, lats } from "api";
+import { getRandomImage } from "placeholder";
+import { BsKey } from "react-icons/bs";
 
 const Edit = () => {
   const { getParam } = useRouting();
@@ -70,7 +80,20 @@ const Edit = () => {
     <BookingsHistorySection />
   );
 
+  const posts = [...Array(10)].map(() => ({
+    id: randomNum(500000).toString(),
+    description: "Lorem",
+    comments: randomNum(500000),
+    likes: randomNum(50000),
+    views: randomNum(50000),
+    shares: randomNum(5000),
+    thumbnail: getRandomImage(),
+    type: "image",
+    createdAt: new Date(),
+  }));
+
   const productsTitle = isProducts ? "Products" : "services";
+  const historyTitle = isProducts ? "Orders" : "Bookings";
 
   const tabsTitles = [
     "General",
@@ -78,7 +101,8 @@ const Edit = () => {
     "Affiliation",
     productsTitle,
     "Transactions",
-    "Invoices",
+    historyTitle,
+    "Social Info",
   ];
 
   return (
@@ -111,6 +135,81 @@ const Edit = () => {
           <div>transations</div>
           <div>
             <div>{invoiceComp}</div>
+            <Pagination />
+          </div>
+          <div>
+            <SocialProfile
+              profileInfo={{
+                accountType: "seller",
+                userId: "1325",
+                id: "1230",
+                name: "Jane Daniel",
+                public: true,
+                thumbnail: "/shop-2.jpeg",
+                verified: true,
+                bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend diam cras eu felis egestas aliquam. Amet ornare",
+                isFollowed: false,
+                links: ["this is a test link"],
+                location: {
+                  address: "address",
+                  city: "city",
+                  cords: {
+                    lat: 32,
+                    lng: 23,
+                  },
+                  country: "country",
+                  countryCode: "CH",
+                  postalCode: 1234,
+                  state: "Geneve",
+                },
+                profileCoverPhoto: "/shop-2.jpeg",
+                publications: 156,
+                subscribers: 135,
+                subscriptions: 14,
+                profession: "Agent",
+              }}
+            />
+            <Table className="w-full">
+              <THead>
+                <Tr>
+                  <Th>{t("Thumbnail")}</Th>
+                  <Th>{t("ID")}</Th>
+                  <Th>{t("Legend")}</Th>
+                  <Th>{t("Views")}</Th>
+                  <Th>{t("Likes")}</Th>
+                  <Th>{t("Comments")}</Th>
+                  <Th>{t("Shares")}</Th>
+                  <Th>{t("Publish Date")}</Th>
+                  <Th>{t("Action")}</Th>
+                </Tr>
+              </THead>
+              <TBody>
+                {mapArray(posts, (data, i) => (
+                  <Tr key={i}>
+                    <Td className="w-fit">
+                      {data.type === "video" ? (
+                        <></>
+                      ) : (
+                        <Image className="w-32" src={data.thumbnail} />
+                      )}
+                    </Td>
+                    <Td>{data.id.slice(0, 4)}...</Td>
+                    <Td>{data.description.slice(0, 10)}...</Td>
+                    <Td>{data.views}</Td>
+                    <Td>{data.likes}</Td>
+                    <Td>{data.comments}</Td>
+                    <Td>{data.shares}</Td>
+                    <Td>{new Date(data.createdAt).toDateString()}</Td>
+                    <Td className="text-white">
+                      <div className="flex flex-wrap gap-2">
+                        <BsKey className="w-8 h-8 p-2 bg-green-500" />
+                        <TrashIcon className="w-8 h-8 p-2 bg-red-500" />
+                      </div>
+                    </Td>
+                  </Tr>
+                ))}
+              </TBody>
+            </Table>
             <Pagination />
           </div>
         </SimpleTabItemList>
