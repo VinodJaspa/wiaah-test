b14a26f36c3d8b5614569f7bb905ec96e6175824
kerlos <kero24862@gmail.com>
2022-12-26 14:04:27 +0200

    country banning w/Testing

--- apps/back-end/services/moderation/src/banned-countries/banned-cities.spec.ts
+++ apps/back-end/services/moderation/src/banned-countries/banned-cities.spec.ts
@@ -4,10 +4,13 @@ import { PrismaService } from 'prismaService';
 import { AppModule } from '../app.module';
 import { ObjectId } from 'mongodb';
 import {
+  accountType,
   AuthorizationDecodedUser,
+  MockedAdminUser,
   mockedUser,
   requestGraphql,
 } from 'nest-utils';
+import { BanCitiesInput } from './dto/create-banned-country.input';
 
 describe('country banning', () => {
   let app: INestApplication;
@@ -65,12 +68,66 @@ describe('country banning', () => {
 
   it('Should ban buyer city', async () => {
     let cities = await prisma.bannedCity.findMany();
+    let input: BanCitiesInput = {
+      citiesIds: [new ObjectId().toHexString(), new ObjectId().toHexString()],
+    };
     expect(cities).toHaveLength(0);
 
-    const res = await reqGql(banbuyersquery, {}, mockedUser);
+    let res = await reqGql(banbuyersquery, input, mockedUser);
 
     cities = await prisma.bannedCity.findMany();
     expect(res.body.errors).toBeDefined();
+    expect(cities).toHaveLength(0);
+
+    res = await reqGql(banbuyersquery, input, MockedAdminUser);
+
+    cities = await prisma.bannedCity.findMany();
+    expect(res.body.errors).not.toBeDefined();
+    expect(cities).toHaveLength(2);
+    expect(
+      cities.every(
+        (v) =>
+          input.citiesIds.includes(v.cityId) &&
+          v.bannedFor === accountType.BUYER,
+      ),
+    ).toBe(true);
+
+    res = await reqGql(unbanbuyersquery, input, MockedAdminUser);
+
+    cities = await prisma.bannedCity.findMany();
+    expect(res.body.errors).not.toBeDefined();
+    expect(cities).toHaveLength(0);
+  });
+  it('Should ban seller city', async () => {
+    let cities = await prisma.bannedCity.findMany();
+    let input: BanCitiesInput = {
+      citiesIds: [new ObjectId().toHexString(), new ObjectId().toHexString()],
+    };
+    expect(cities).toHaveLength(0);
+
+    let res = await reqGql(bansellersquery, input, mockedUser);
+
+    cities = await prisma.bannedCity.findMany();
+    expect(res.body.errors).toBeDefined();
+    expect(cities).toHaveLength(0);
+
+    res = await reqGql(bansellersquery, input, MockedAdminUser);
+
+    cities = await prisma.bannedCity.findMany();
+    expect(res.body.errors).not.toBeDefined();
+    expect(cities).toHaveLength(2);
+    expect(
+      cities.every(
+        (v) =>
+          input.citiesIds.includes(v.cityId) &&
+          v.bannedFor === accountType.SELLER,
+      ),
+    ).toBe(true);
+
+    res = await reqGql(unbansellersquery, input, MockedAdminUser);
+
+    cities = await prisma.bannedCity.findMany();
+    expect(res.body.errors).not.toBeDefined();
     expect(cities).toHaveLength(0);
   });
 });
---
+++ apps/back-end/services/moderation/test/setup/setupFile.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from 'prismaClient';
+import { Client } from '@elastic/elasticsearch';
+import { config } from 'dotenv';
+config();
+
+class PrismaService extends PrismaClient {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
+
+async function clearDB() {
+  await prisma.hotelRoom.deleteMany();
+  await prisma.hotelService.deleteMany();
+  await prisma.serviceCategory.deleteMany();
+  await prisma.restaurantService.deleteMany();
+  await prisma.restaurantEstablishmentType.deleteMany();
+  await prisma.restaurantCuisinesType.deleteMany();
+  await prisma.restaurnatSettingAndAmbiance.deleteMany();
+  await prisma.serviceOwnerShip.deleteMany();
+  await prisma.healthCenterDoctor.deleteMany();
+  await prisma.healthCenterService.deleteMany();
+  await prisma.healthCenterSpecialty.deleteMany();
+  await prisma.beautyCenterService.deleteMany();
+  await prisma.beautyCenterTreatmentCategory.deleteMany();
+  await prisma.serviceWorkingSchedule.deleteMany();
+
+  client.deleteByQuery({
+    index: '*',
+    query: {
+      match_all: {},
+    },
+  });
+}
+
+const client = new Client({
+  node: process.env.ELASTIC_HOST_TEST,
+  auth: {
+    username: process.env.ELASTIC_USERNAME_TEST,
+    password: process.env.ELASTIC_PASSWORD_TEST,
+  },
+});
+const prisma = new PrismaService();
+beforeAll(async () => {
+  await clearDB();
+});
+
+afterEach(async () => {
+  await clearDB();
+});
+
+afterAll(async () => {
+  await clearDB();
+  prisma.$disconnect();
+});
