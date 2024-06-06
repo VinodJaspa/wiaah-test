4d6591ac0b803e40bf22daca724386930757d108
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-06 16:06:05 +0300

    refactor: Convert chakra tags to regular html tags and tailwind (cause it takes a long time to render)

--- apps/front-end/market/components/CartSummary/EmptyCartSummary.tsx
+++ apps/front-end/market/components/CartSummary/EmptyCartSummary.tsx
@@ -1,30 +1,22 @@
 import { Flex, Text, Button } from "@chakra-ui/react";
-import { t } from "i18next";
 import React from "react";
 import { colorPalette } from "ui/components/helpers/colorPalette";
 import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
 
 const EmptyCartSummary: React.FC = () => {
+  const { t } = useTranslation(); // Destructure the translate function (t)
   const router = useRouter();
   return (
-    <Flex
-      justifyContent={"space-between"}
-      direction={"column"}
-      alignItems={"end"}
-      h="100%"
-    >
-      <Text w="100%">There are no items in this cart!</Text>
-      <Button
-        rounded={"none"}
-        _hover={{ bgColor: colorPalette.PrimaryGreen }}
-        color={colorPalette.whiteText}
-        w="fit-content"
-        bgColor={colorPalette.PrimaryGreen}
+    <div className="flex flex-col items-end h-full justyf-between ">
+      <text className="w-full">There are no items in this cart!</text>
+      <button
+        className={` w-fit bg-[${colorPalette.PrimaryGreen}] hover:bg-[${colorPalette.PrimaryGreen}] text-[${colorPalette.whiteText}`}
         onClick={() => router.push("/")}
       >
-        {t("continue_shopping", "Continue Shopping")}
-      </Button>
-    </Flex>
+        {t("continue_shopping, Continue Shopping")}
+      </button>
+    </div>
   );
 };
 
