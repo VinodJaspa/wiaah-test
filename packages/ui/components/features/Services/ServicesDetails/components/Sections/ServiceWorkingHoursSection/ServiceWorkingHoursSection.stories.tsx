c9a5396408402fe56204564e2493e4bb1989a9c7
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-09 19:41:03 +0300

    fix: Put the right workingHours data type to ServcieWorkingHoursSection

--- packages/ui/components/features/Services/ServicesDetails/components/Sections/ServiceWorkingHoursSection/ServiceWorkingHoursSection.stories.tsx
+++ packages/ui/components/features/Services/ServicesDetails/components/Sections/ServiceWorkingHoursSection/ServiceWorkingHoursSection.stories.tsx
@@ -2,94 +2,30 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { storybookOtherServicesSectionsTitle } from "utils";
 import { ServiceWorkingHoursSection } from "./ServiceWorkingHoursSection";
 
-export default {
+const meta: ComponentMeta<typeof ServiceWorkingHoursSection> = {
   title: storybookOtherServicesSectionsTitle + "ServiceWorkingHoursSection",
   component: ServiceWorkingHoursSection,
-} as ComponentMeta<typeof ServiceWorkingHoursSection>;
+};
 
-const template: ComponentStory<typeof ServiceWorkingHoursSection> = (args) => (
+export default meta;
+
+const Template: ComponentStory<typeof ServiceWorkingHoursSection> = (args) => (
   <ServiceWorkingHoursSection {...args} />
 );
 
-export const Default = template.bind({});
+export const Default: ComponentStory<typeof ServiceWorkingHoursSection> =
+  Template.bind({});
 Default.args = {
-  workingDays: [
-    {
-      weekDay: "Friday",
-      from: {
-        hour: 0,
-        minutes: 0,
-      },
-      to: {
-        hour: 0,
-        minutes: 0,
-      },
+  workingHours: {
+    id: "1",
+    weekdays: {
+      mo: { periods: ["2023-06-09T09:00:00.000Z", "2023-06-09T19:30:00.000Z"] },
+      tu: { periods: ["2023-06-10T09:00:00.000Z", "2023-06-10T19:30:00.000Z"] },
+      we: { periods: ["2023-06-11T09:00:00.000Z", "2023-06-11T19:30:00.000Z"] },
+      th: { periods: ["2023-06-12T09:00:00.000Z", "2023-06-12T19:30:00.000Z"] },
+      fr: { periods: ["2023-06-13T00:00:00.000Z", "2023-06-13T00:00:00.000Z"] },
+      sa: { periods: ["2023-06-14T09:00:00.000Z", "2023-06-14T19:30:00.000Z"] },
+      su: { periods: ["2023-06-15T09:00:00.000Z", "2023-06-15T19:30:00.000Z"] },
     },
-    {
-      weekDay: "Monday",
-      from: {
-        hour: 9,
-        minutes: 0,
-      },
-      to: {
-        hour: 19,
-        minutes: 30,
-      },
-    },
-    {
-      weekDay: "Saturday",
-      from: {
-        hour: 9,
-        minutes: 0,
-      },
-      to: {
-        hour: 19,
-        minutes: 30,
-      },
-    },
-    {
-      weekDay: "Sunday",
-      from: {
-        hour: 9,
-        minutes: 0,
-      },
-      to: {
-        hour: 19,
-        minutes: 30,
-      },
-    },
-    {
-      weekDay: "Thursday",
-      from: {
-        hour: 9,
-        minutes: 0,
-      },
-      to: {
-        hour: 19,
-        minutes: 30,
-      },
-    },
-    {
-      weekDay: "Tuesday",
-      from: {
-        hour: 9,
-        minutes: 0,
-      },
-      to: {
-        hour: 19,
-        minutes: 30,
-      },
-    },
-    {
-      weekDay: "Wednesday",
-      from: {
-        hour: 9,
-        minutes: 0,
-      },
-      to: {
-        hour: 19,
-        minutes: 30,
-      },
-    },
-  ],
+  },
 };
