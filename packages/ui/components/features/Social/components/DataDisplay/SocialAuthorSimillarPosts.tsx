588228586728383ccff5ba8675588b49420da3e6
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-14 19:07:02 +0300

    feat: Passed data prop to SocialAuthorSimilarPosts

--- packages/ui/components/features/Social/components/DataDisplay/SocialAuthorSimillarPosts.tsx
+++ packages/ui/components/features/Social/components/DataDisplay/SocialAuthorSimillarPosts.tsx
@@ -1,13 +1,18 @@
-import { useGetSocialPostQuery } from "@features/Social/services";
+import {
+  useGetSocialPostQuery,
+  GetSocialPostQuery,
+} from "@features/Social/services";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
 export const SocialAuthorSimillarPosts: React.FC<{
   postId: string;
-}> = ({ postId }) => {
+  data: GetSocialPostQuery["getSocialPostById"];
+}> = ({ postId, data }) => {
   const { t } = useTranslation();
 
-  const { data } = useGetSocialPostQuery({ id: postId });
+  //WARNING: This grqphql is not ready yet so  I replaced it with placeholder
+  const { data: _data } = useGetSocialPostQuery({ id: postId });
   return (
     <div>
       <p className="text-center text-lg font-semibold">
