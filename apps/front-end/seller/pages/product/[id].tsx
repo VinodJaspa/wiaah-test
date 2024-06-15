045de0d90ca4ca5e0641f68a0bafafbf1bd9f9cc
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-15 18:04:09 +0300

    fix:Added PorductView component

--- apps/front-end/seller/pages/product/[id].tsx
+++ apps/front-end/seller/pages/product/[id].tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { ProductView } from "ui/components/features/Products/views/ProductView";
+import { SellerLayout } from "@blocks";
+import { useRouting } from "routing";
 
-const ProductView = () => {
-  return <></>;
+const SellerProductView = () => {
+  const { getParam } = useRouting();
+  const id = getParam("id");
+  return (
+    <SellerLayout>
+      <ProductView productId={id} />
+    </SellerLayout>
+  );
 };
 
-export default ProductView;
+export default SellerProductView;
