7b65ec856692acb4bf8c1543d125a671d64f847e
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-17 16:21:35 +0300

    fix: Fixed type issues in ServicePostView

--- apps/front-end/seller/components/views/Social/ServicePostView.tsx
+++ apps/front-end/seller/components/views/Social/ServicePostView.tsx
@@ -1,3 +1,5 @@
+import { PresentationType, ServicePresentationType } from "@features/API";
+import { getRandomImage } from "placeholder";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import {
@@ -10,15 +12,62 @@ import {
   SocialProfileServicePosts,
 } from "ui";
 
+const FAKE_DEATAILED_POST = {
+  __typename: "ServicePost",
+  id: "post2",
+  reactionNum: 89,
+  shares: 15,
+  comments: 4,
+  createdAt: "2023-06-01T10:20:30Z",
+  userId: "user2",
+  serviceId: "service2",
+  serviceType: "Electrician",
+  views: 875,
+  location: {
+    __typename: "PostLocation",
+    address: "456 Elm St",
+    city: "Monroe",
+    state: "WI",
+    country: "USA",
+  },
+  service: {
+    __typename: "Service",
+    id: "service2",
+    title: "Electrical Service",
+    thumbnail: getRandomImage(),
+    price: 200,
+    rating: 4.8,
+  },
+  user: {
+    __typename: "Account",
+    id: "user2",
+    profile: {
+      __typename: "Profile",
+      id: "profile2",
+      username: "jane_smith",
+      photo: getRandomImage(),
+      profession: "Electrician",
+      verified: false,
+      followers: 120,
+    },
+  },
+};
+
 export const ServicePostView: React.FC<{ id: string }> = ({ id }) => {
   const { t } = useTranslation();
-  const { data, isLoading, isError } = useGetServicePostDetails(id);
+  // WARNING: graphql query is not ready yet
+  const {
+    data: _data,
+    isLoading: _isloading,
+    isError: _isError,
+  } = useGetServicePostDetails(id);
+  const data = FAKE_DEATAILED_POST;
 
   return (
     <div className="py-2 md:py-16 gap-8 flex flex-col">
       <div className="flex items-center md:items-start flex-col gap-8 mb-24 md:flex-row">
         <div className="flex flex-col gap-4">
-          <SpinnerFallback isError={isError} isLoading={isLoading}>
+          <SpinnerFallback isLoading={false}>
             {data ? (
               <SocialPostHeader
                 name={data.user.profile.username}
@@ -26,14 +75,32 @@ export const ServicePostView: React.FC<{ id: string }> = ({ id }) => {
               />
             ) : null}
           </SpinnerFallback>
-          <ShowMapButton onClick={() => {}} />
+          <ShowMapButton onClick={() => { }} />
         </div>
-        <SpinnerFallback isError={isError} isLoading={isLoading}>
-          {data ? <SocialServicePostDetailsCard post={data} /> : null}
+        <SpinnerFallback isLoading={false}>
+          {data ? (
+            <SocialServicePostDetailsCard
+              post={{
+                id: data.id,
+                comments: data.comments,
+                reactionNum: data.reactionNum,
+                service: {
+                  presentation: [
+                    {
+                      type: ServicePresentationType.Img,
+                      src: data.service.thumbnail,
+                    },
+                  ],
+                  title: data.service.title,
+                  hashtags: ["fake_hashtag"],
+                },
+              }}
+            />
+          ) : null}
         </SpinnerFallback>
       </div>
       <p className="text-3xl font-bold w-full text-center">
-        <SpinnerFallback isError={isError} isLoading={isLoading}>
+        <SpinnerFallback isLoading={false}>
           {data ? (
             <>
               {t("view")} {data.user.profile.username} {t("other posts")}
