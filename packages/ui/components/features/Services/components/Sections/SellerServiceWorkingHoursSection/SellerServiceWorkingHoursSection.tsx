96f9abca8c20f604223d000f53fe2dcf77df161b
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-17 16:29:07 +0300

    style: Changed SellerServiceWrokingHOursSection cause it overlap with calender bar

--- packages/ui/components/features/Services/components/Sections/SellerServiceWorkingHoursSection/SellerServiceWorkingHoursSection.tsx
+++ packages/ui/components/features/Services/components/Sections/SellerServiceWorkingHoursSection/SellerServiceWorkingHoursSection.tsx
@@ -13,7 +13,7 @@ export const SellerServiceWorkingHoursSection: React.FC<
 > = ({ workingDays }) => {
   const { t } = useTranslation();
   return (
-    <div className="flex gap-4 items-center">
+    <div className="flex gap-4 items-center flex-wrap w-fit">
       {mapArray(workingDays, (day, i) => {
         const today = i === 1 ? true : false;
         const fromDate = DateDetails(new Date(day.periods[0]));
@@ -28,9 +28,8 @@ export const SellerServiceWorkingHoursSection: React.FC<
               {today ? t("Today") : ""}
             </p>
             <div
-              className={`${
-                today ? "border-2 border-primary rounded-xl" : ""
-              } pt-[0.625rem] pb-6 text-lg px-4 flex flex-col items-center gap-8`}
+              className={`${today ? "border-2 border-primary rounded-xl" : ""
+                } pt-[0.625rem] pb-6 text-lg px-4 flex flex-col items-center gap-8`}
             >
               <p className="text-sm text-center text-lightBlack font-semibold">
                 {new Date(day.periods[0]).toLocaleDateString("en", {
@@ -45,9 +44,8 @@ export const SellerServiceWorkingHoursSection: React.FC<
                 />
               </div>
               <div
-                className={`whitespace-nowrap text-center ${
-                  dayoff ? "text-grayText" : "text-primary"
-                } font-semibold text-sm`}
+                className={`whitespace-nowrap text-center ${dayoff ? "text-grayText" : "text-primary"
+                  } font-semibold text-sm`}
               >
                 {dayoff
                   ? t("Day Off")
