1498af86275fc4f348ca416ac208227948c2957c
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-17 16:31:19 +0300

    fix: Fixed ShopSearchView type issues & added placeholder

--- packages/ui/components/features/Shop/views/ShopsSearchView.tsx
+++ packages/ui/components/features/Shop/views/ShopsSearchView.tsx
@@ -2,13 +2,53 @@ import { Image, Slider } from "@partials";
 import { SectionHeader } from "@sections/ShoppingManagement";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useGetFilteredShopsQuery } from "../services";
+// import { useGetFilteredShopsQuery } from "../services";
 import { mapArray, useForm } from "@UI/../utils/src";
 import { usePaginationControls } from "@blocks";
 import { useResponsive } from "@src/index";
 import { StoreType } from "@features/API";
 import { startCase } from "lodash";
 import { FilterIcon } from "@UI/components/partials/icons/FilterIcon";
+import { getRandomImage } from "placeholder";
+
+const FAKE_SHOPS_DATA = [
+  {
+    id: "1",
+    thumbnail: getRandomImage(),
+    type: "Electronics",
+    storeType: StoreType.Service,
+    storeCategory: "Home Appliances",
+  },
+
+  {
+    id: "2",
+    thumbnail: getRandomImage(),
+    type: "Electronics",
+    storeType: StoreType.Service,
+    storeCategory: "Home Appliances",
+  },
+  {
+    id: "3",
+    thumbnail: getRandomImage(),
+    type: "Electronics",
+    storeType: StoreType.Service,
+    storeCategory: "Home Appliances",
+  },
+  {
+    id: "4",
+    thumbnail: getRandomImage(),
+    type: "Electronics",
+    storeType: StoreType.Service,
+    storeCategory: "Home Appliances",
+  },
+  {
+    id: "5",
+    thumbnail: getRandomImage(),
+    type: "Electronics",
+    storeType: StoreType.Service,
+    storeCategory: "Home Appliances",
+  },
+];
 
 export const ShopsSearchView: React.FC<{
   slug: string;
@@ -16,23 +56,31 @@ export const ShopsSearchView: React.FC<{
   const { t } = useTranslation();
   const { controls, pagination } = usePaginationControls();
   const { isMobile } = useResponsive();
-  const { form } = useForm<Parameters<typeof useGetFilteredShopsQuery>[0]>({
-    pagination,
-    searchQuery: slug,
-  });
-  const { data: shops } = useGetFilteredShopsQuery(form);
+  // WARNING: this grqphql endpoint is not ready yet
+  // const { form } = useForm<Parameters<typeof useGetFilteredShopsQuery>[0]>({
+  //   pagination,
+  //   searchQuery: slug,
+  // });
+  //
+  //WARNING: graphql server is not ready yet & this specific endpoint is not created yet
+  // const { data: shops } = useGetFilteredShopsQuery(form);
+
+  const shops = FAKE_SHOPS_DATA;
 
   return isMobile ? (
     <div className="flex flex-col gap-4">
       <SectionHeader sectionTitle={t("Shops & Services")}>
-        <button onClick={() => {}}>
+        <button onClick={() => { }}>
           <FilterIcon className="text-2xl" />
         </button>
       </SectionHeader>
 
       <Slider>
         {mapArray(shops, (shop, i) => (
-          <div key={shop.id + i} className="relative w-11/12 h-5/6">
+          <div
+            key={shop.id + i}
+            className="relative w-11/12 h-full object-cover"
+          >
             <Image src={shop.thumbnail} className="w-full h-full" />
 
             <div className="absolute bg-primary top-8 left-0">
