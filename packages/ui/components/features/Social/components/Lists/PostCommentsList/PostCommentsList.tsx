1e0f9fd7120859636eefad06c3ed526da1ab9aae
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-18 21:08:32 +0300

    feat: Added placeholder to PostCommentsList component

--- packages/ui/components/features/Social/components/Lists/PostCommentsList/PostCommentsList.tsx
+++ packages/ui/components/features/Social/components/Lists/PostCommentsList/PostCommentsList.tsx
@@ -4,7 +4,55 @@ import {
   SpinnerFallback,
   useGetContentCommentsQuery,
 } from "@UI";
-import { Comment } from "@features/API";
+import { AttachmentType, Comment, ContentHostType } from "@features/API";
+import { getRandomImage } from "placeholder";
+
+const FAKE_CONTENT_COMMENT = {
+  data: [
+    {
+      id: "comment1",
+      content: "This is a sample comment content.",
+      commentedAt: "2024-06-17T12:00:00Z",
+      likes: 10,
+      userId: "user1",
+      hostId: "post1",
+      hostType: ContentHostType.Comment,
+      updatedAt: "2024-06-17T12:05:00Z",
+      replies: 7,
+      attachment: {
+        src: getRandomImage(),
+        type: AttachmentType.Img,
+      },
+      author: {
+        username: "user1",
+        photo: "https://example.com/photo.jpg",
+        verified: true,
+        id: "profile1",
+      },
+    },
+    {
+      id: "comment2",
+      content: "Another example comment.",
+      commentedAt: "2024-06-17T13:00:00Z",
+      likes: 5,
+      userId: "user2",
+      hostId: "post1",
+      hostType: ContentHostType.Comment,
+      updatedAt: "2024-06-17T13:10:00Z",
+      replies: 6,
+      attachment: {
+        src: getRandomImage(),
+        type: AttachmentType.Img,
+      },
+      author: {
+        username: "user2",
+        photo: getRandomImage(),
+        verified: true,
+        id: "profile2",
+      },
+    },
+  ],
+};
 
 export interface PostCommentsListProps {
   postId: string;
@@ -13,23 +61,30 @@ export const PostCommentsList: React.FC<PostCommentsListProps> = ({
   postId,
 }) => {
   const [cursor, setCursor] = React.useState<string>();
-  const { isLoading, isError, data } = useGetContentCommentsQuery({
+  const {
+    isLoading,
+    isError,
+    data: _data,
+  } = useGetContentCommentsQuery({
     id: postId,
     take: 10,
     cursor,
   });
+  const data = FAKE_CONTENT_COMMENT;
 
   return (
     <SpinnerFallback isLoading={isLoading} isError={isError}>
-      {data ? (
+      {data.data ? (
         <CommentsViewer
           maxInitailComments={4}
-          comments={
-            data.pages.reduce(
-              (acc, curr) => acc.concat(curr.data || []),
-              [] as Comment[]
-            ) || []
-          }
+          comments={data.data}
+        //NOTE: use this when the graphql query is ready
+        // comments={
+        //   data.pages.reduce(
+        //     (acc, curr) => acc.concat(curr.data || []),
+        //     [] as Comment[]
+        //   ) || []
+        // }
         />
       ) : null}
     </SpinnerFallback>
