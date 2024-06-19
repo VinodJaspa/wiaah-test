744b8af9ceec5b257c1e47629f465d3af8157512
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-19 16:04:09 +0300

    fix: Fixed hashtag apge Head title

--- apps/front-end/buyer/pages/hashtag/index.tsx
+++ apps/front-end/buyer/pages/hashtag/index.tsx
@@ -5,29 +5,30 @@ import { SellerLayout, useResponsive } from "ui";
 import { dehydrate, QueryClient } from "react-query";
 import { HashtagsView } from "../../components";
 
-interface HashtagPageProps {}
+interface HashtagPageProps { }
 function getHashtags() {
   return [];
 }
-export const getServerSideProps: GetServerSideProps<HashtagPageProps> =
-  async () => {
-    const queryClient = new QueryClient();
+export const getServerSideProps: GetServerSideProps<
+  HashtagPageProps
+> = async () => {
+  const queryClient = new QueryClient();
 
-    queryClient.prefetchQuery("hashtags", getHashtags);
+  queryClient.prefetchQuery("hashtags", getHashtags);
 
-    return {
-      props: {
-        dehydratedState: dehydrate(queryClient),
-      },
-    };
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
   };
+};
 
 const HashtagPage: NextPage<HashtagPageProps> = () => {
   const { isMobile } = useResponsive();
   return (
     <>
       <Head>
-        <title>seller | hashtags</title>
+        <title>Buyer | hashtags</title>
       </Head>
       <SellerLayout header={isMobile ? "minimal" : "main"}>
         <HashtagsView />
