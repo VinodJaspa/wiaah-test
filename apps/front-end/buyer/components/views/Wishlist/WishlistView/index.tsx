a358b99e9e7259dabcaa3ccbbd27261b7a32ec81
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-20 18:47:26 +0300

    feat: Added placeholder & fixed issues

--- apps/front-end/buyer/components/views/Wishlist/WishlistView/index.tsx
+++ apps/front-end/buyer/components/views/Wishlist/WishlistView/index.tsx
@@ -12,11 +12,18 @@ import {
   SpinnerFallback,
 } from "ui";
 import WishlistEmpty from "../WishlistEmpty";
+import { getRandomImage } from "placeholder";
 
 export const WishlistView: React.FC = () => {
   const { OpenLoginPopup } = useLoginPopup();
 
-  const { data: savedItems, isLoading, isError } = useGetWishlistItemsData();
+  // const {
+  //   data: _savedItems,
+  //   isLoading: _isLoading,
+  //   isError: _isError,
+  // } = useGetWishlistItemsData();
+
+  const savedItems = FAKE_WISHLIST_DATA;
 
   function handleAddToCart(item: WishlistItem) {
     handleLoginPopup();
@@ -30,7 +37,7 @@ export const WishlistView: React.FC = () => {
       <span className="flex h-16 pb-4 w-full items-center justify-center text-3xl font-bold">
         Saved Items
       </span>
-      <SpinnerFallback isLoading={isLoading} isError={isError}>
+      <SpinnerFallback isLoading={false}>
         <GridContainerPager showPagination={false} componentsLimit={12}>
           {savedItems &&
             savedItems.map((item, i) => {
@@ -38,14 +45,17 @@ export const WishlistView: React.FC = () => {
               return (
                 <ProductCard
                   key={i}
+                  id={item.id}
+                  price={item.price}
+                  discount={item.discount}
                   onButtonClick={() => handleAddToCart(item)}
                   onDelete={() => handleLoginPopup()}
-                  postion="delete"
+                  position="delete"
                   forceHover={true}
                   buttonText={type > 50 ? "Add to Cart" : "Book now"}
-                  {...item}
-                  imageUrl={item.imgUrl}
-                  variant={type > 50 ? "product" : "service"}
+                  cashback={parseInt(item.cashback)}
+                  thumbnail={item.imgUrl}
+                  rate={item.rating}
                 />
               );
             })}
@@ -55,3 +65,85 @@ export const WishlistView: React.FC = () => {
     </div>
   );
 };
+
+const FAKE_WISHLIST_DATA: WishlistItem[] = [
+  {
+    id: "placeholder-id-5",
+    name: "Placeholder Name",
+    price: 80,
+    imgUrl: getRandomImage(),
+    type: "product",
+    currency: "USD",
+    currencySymbol: "$",
+    colors: ["#000000", "#FFFFFF"],
+    liked: false,
+    cashback: "0%",
+    discount: 0,
+    oldPrice: 100,
+    rating: 0,
+  },
+
+  {
+    id: "placeholder-id-4",
+    name: "Placeholder Name",
+    price: 30,
+    imgUrl: getRandomImage(),
+    type: "product",
+    currency: "USD",
+    currencySymbol: "$",
+    colors: ["#000000", "#FFFFFF"],
+    liked: false,
+    cashback: "20%",
+    discount: 10,
+    oldPrice: 50,
+    rating: 3,
+  },
+
+  {
+    id: "placeholder-id-3",
+    name: "Placeholder Name",
+    price: 40,
+    imgUrl: getRandomImage(),
+    type: "product",
+    currency: "USD",
+    currencySymbol: "$",
+    colors: ["#000000", "#FFFFFF"],
+    liked: false,
+    cashback: "0%",
+    discount: 0,
+    oldPrice: 50,
+    rating: 0,
+  },
+
+  {
+    id: "placeholder-id-2",
+    name: "Placeholder Name",
+    price: 0,
+    imgUrl: getRandomImage(),
+    type: "product",
+    currency: "USD",
+    currencySymbol: "$",
+    colors: ["#000000", "#FFFFFF"],
+    liked: false,
+    cashback: "0%",
+    discount: 0,
+    oldPrice: 0,
+    rating: 5,
+  },
+
+  {
+    id: "placeholder-id-1",
+    name: "Placeholder Name",
+    price: 80,
+    imgUrl: getRandomImage(),
+    type: "product",
+    currency: "USD",
+    currencySymbol: "$",
+    colors: ["#000000", "#FFFFFF"],
+    liked: false,
+    cashback: "0%",
+    discount: 0,
+    oldPrice: 100,
+    rating: 0,
+  },
+];
