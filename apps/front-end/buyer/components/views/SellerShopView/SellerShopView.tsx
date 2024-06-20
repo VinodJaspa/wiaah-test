9bf9ecd514ae47e355ef0760e81bd82119c5bc9e
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-20 18:45:50 +0300

    fix: passed the correct placeholder

--- apps/front-end/buyer/components/views/SellerShopView/SellerShopView.tsx
+++ apps/front-end/buyer/components/views/SellerShopView/SellerShopView.tsx
@@ -8,10 +8,15 @@ import {
   ProductViewModal,
   useProductViewModal,
 } from "ui";
-import { ShopCardsInfoPlaceholder } from "ui/placeholder/social";
+import {
+  ShopCardsInfoPlaceholder,
+  SocialShopsPostCardPlaceholder,
+} from "ui/placeholder/social";
 import { useTranslation } from "react-i18next";
 import { useRouter } from "next/router";
 import { ShopCardInfo } from "types";
+import { CashbackType, PresentationType } from "@features/API";
+import { getRandomImage } from "placeholder";
 
 export const SellerShopView: React.FC = () => {
   const { t } = useTranslation();
@@ -48,21 +53,192 @@ export const SellerShopView: React.FC = () => {
             );
           }}
         />
-        <Box w="100%">
+        <div className="flex flex-col w-full h-full gap-8">
           <ShopFilter onlyMobile={false} />
-          <ShopCardsListWrapper
-            onCardClick={(id) => {
-              // router.push(
-              //   router.pathname,
-              //   { query: { shopPostId: id } },
-              //   { shallow: true }
-              // );
-            }}
-            cols={cols}
-            items={ShopCardsInfoPlaceholder}
-          />
-        </Box>
+          <ShopCardsListWrapper cols={cols} items={FAKE_SHOP_POST} />
+        </div>
       </VStack>
     </>
   );
 };
+
+const FAKE_SHOP_POST = [
+  {
+    postInfo: {
+      id: "post1",
+      comments: 25,
+      shares: 10,
+      reactionNum: 50,
+      userId: "user123",
+      createdAt: "2023-06-01T00:00:00Z",
+      product: {
+        id: "product1",
+        presentations: [
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+        ],
+        title: "Placeholder Product",
+        hashtags: ["#placeholder", "#product"],
+        price: 29.99,
+        cashback: {
+          amount: 20,
+          id: "cashback1",
+          type: CashbackType.Cash,
+          units: 100,
+        },
+        discount: {
+          amount: 10,
+          id: "discount1",
+          units: 5,
+        },
+      },
+    },
+    profileInfo: {
+      id: "profile1",
+      verified: true,
+      photo: getRandomImage(),
+      username: "user123",
+      profession: "Software Developer",
+    },
+  },
+
+  {
+    postInfo: {
+      id: "post1",
+      comments: 25,
+      shares: 10,
+      reactionNum: 50,
+      userId: "user123",
+      createdAt: "2023-06-01T00:00:00Z",
+      product: {
+        id: "product1",
+        presentations: [
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+        ],
+        title: "Placeholder Product",
+        hashtags: ["#placeholder", "#product"],
+        price: 29.99,
+        cashback: {
+          amount: 20,
+          id: "cashback1",
+          type: CashbackType.Cash,
+          units: 100,
+        },
+        discount: {
+          amount: 10,
+          id: "discount1",
+          units: 5,
+        },
+      },
+    },
+    profileInfo: {
+      id: "profile1",
+      verified: true,
+      photo: getRandomImage(),
+      username: "user123",
+      profession: "Software Developer",
+    },
+  },
+
+  {
+    postInfo: {
+      id: "post1",
+      comments: 25,
+      shares: 10,
+      reactionNum: 50,
+      userId: "user123",
+      createdAt: "2023-06-01T00:00:00Z",
+      product: {
+        id: "product1",
+        presentations: [
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+        ],
+        title: "Placeholder Product",
+        hashtags: ["#placeholder", "#product"],
+        price: 29.99,
+        cashback: {
+          amount: 20,
+          id: "cashback1",
+          type: CashbackType.Cash,
+          units: 100,
+        },
+        discount: {
+          amount: 10,
+          id: "discount1",
+          units: 5,
+        },
+      },
+    },
+    profileInfo: {
+      id: "profile1",
+      verified: true,
+      photo: getRandomImage(),
+      username: "user123",
+      profession: "Software Developer",
+    },
+  },
+  {
+    postInfo: {
+      id: "post1",
+      comments: 25,
+      shares: 10,
+      reactionNum: 50,
+      userId: "user123",
+      createdAt: "2023-06-01T00:00:00Z",
+      product: {
+        id: "product1",
+        presentations: [
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+          {
+            type: PresentationType.Image,
+            src: getRandomImage(),
+          },
+        ],
+        title: "Placeholder Product",
+        hashtags: ["#placeholder", "#product"],
+        price: 29.99,
+        cashback: {
+          amount: 20,
+          id: "cashback1",
+          type: CashbackType.Cash,
+          units: 100,
+        },
+        discount: {
+          amount: 10,
+          id: "discount1",
+          units: 5,
+        },
+      },
+    },
+    profileInfo: {
+      id: "profile1",
+      verified: true,
+      photo: getRandomImage(),
+      username: "user123",
+      profession: "Software Developer",
+    },
+  },
+];
