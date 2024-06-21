3dc62e8372d8f5a002af96ed8076c0ca4e9160f7
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-21 19:03:58 +0300

    fixed: Imported supertest correctly

--- apps/back-end/packages/utils/src/test/requestGraphql.ts
+++ apps/back-end/packages/utils/src/test/requestGraphql.ts
@@ -1,12 +1,12 @@
 import { INestApplication } from "@nestjs/common";
-import * as request from "supertest";
+import supertest from "supertest";
 
 export function requestGraphql(
   app: INestApplication,
   query: string,
   variables: Record<string, any>
 ) {
-  return request(app.getHttpServer()).post("/graphql").send({
+  return supertest(app.getHttpServer()).post("/graphql").send({
     query,
     variables,
   });
