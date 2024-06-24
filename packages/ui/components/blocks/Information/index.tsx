c37cca98c041f3bc0ed69a1719c91348370b806b
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-24 17:38:40 +0300

    fix: Fixed type issues

--- packages/ui/components/blocks/Information/index.tsx
+++ packages/ui/components/blocks/Information/index.tsx
@@ -10,22 +10,22 @@ export const Information: FC = () => {
       <ul className="block space-y-4 text-sm text-gray-400">
         <li>
           <Link href={information.about_wiaah}>
-            {t("About_Wiaah", "About Wiaah")}
+            {t("About_Wiaah", "About Wiaah").toString()}
           </Link>
         </li>
         <li>
           <Link href={information.privacy_policy}>
-            {t("Privacy_Policy", "Privacy Policy")}
+            {t("Privacy_Policy", "Privacy Policy").toString()}
           </Link>
         </li>
         <li>
           <Link href={information.terms_and_conditions}>
-            {t("Terms_Conditions", "Terms & Conditions")}
+            {t("Terms_Conditions", "Terms & Conditions").toString()}
           </Link>
         </li>
         <li>
           <Link href={information.delivery_information}>
-            {t("Delivery Informations")}
+            {t("Delivery Informations").toString()}
           </Link>
         </li>
       </ul>
