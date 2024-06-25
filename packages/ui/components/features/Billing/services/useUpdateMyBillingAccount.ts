d659101d4370ba1c9913cc0c41fea0edee577cd5
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-25 19:16:33 +0300

    fix: Fixed useUpdateMyBillingAccount type errros

--- packages/ui/components/features/Billing/services/useUpdateMyBillingAccount.ts
+++ packages/ui/components/features/Billing/services/useUpdateMyBillingAccount.ts
@@ -1,35 +1,37 @@
 import { CreateBillingAccountInput, Exact, Mutation } from "@features/API";
 import { createGraphqlRequestClient } from "api";
 import { useMutation } from "react-query";
+import { GetMyPayoutAccountQuery } from "./useGetMyPayoutAccount";
 
 export type UpdateBillingAccountMutationVariables = Exact<{
-  args: CreateBillingAccountInput;
+  args: GetMyPayoutAccountQuery["getMyBillingAccount"];
 }>;
 
 export type UpdateBillingAccountMutation = {
   __typename?: "Mutation";
-  updateMyBillingAccount: any; // Adjust return type based on actual response type
+  updateMyBillingAccount: {};
 };
 
 export const useUpdateMyBillingAccountMutation = () =>
-  useMutation<boolean, unknown, UpdateBillingAccountMutationVariables["args"]>(
-    ["update-billing"],
-    async (args) => {
-      const client = createGraphqlRequestClient();
+  useMutation<
+    UpdateBillingAccountMutation["updateMyBillingAccount"],
+    unknown,
+    UpdateBillingAccountMutationVariables["args"]
+  >(["update-billing"], async (args) => {
+    const client = createGraphqlRequestClient();
 
-      const res = await client
-        .setQuery(
-          `
+    const res = await client
+      .setQuery(
+        `
 mutation updateBillingAccount ($args:CreateBillingAccountInput!){
   updateMyBillingAccount(args:$args)
 }
     `
-        )
-        .setVariables<UpdateBillingAccountMutationVariables>({
-          args,
-        })
-        .send<UpdateBillingAccountMutation>();
+      )
+      .setVariables<UpdateBillingAccountMutationVariables>({
+        args,
+      })
+      .send<UpdateBillingAccountMutation>();
 
-      return res.data.updateMyBillingAccount;
-    }
-  );
+    return res.data.updateMyBillingAccount;
+  });
