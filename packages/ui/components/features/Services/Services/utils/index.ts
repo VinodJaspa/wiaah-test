56ac57896fa3482d016d1065558473879a72faf8
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-27 17:34:15 +0300

    fix: Fixed converWrokingSheduleToWorkingHours funtion

--- packages/ui/components/features/Services/Services/utils/index.ts
+++ packages/ui/components/features/Services/Services/utils/index.ts
@@ -1,22 +1,41 @@
-import { WorkingDate } from "types";
+import { DateType, WorkingDate, WorkingHoursRange } from "types";
 import { WorkingSchedule } from "../types";
+import { ServiceDayWorkingHours } from "@features/API/gql/generated";
 
+function isServiceDayWorkingHours(value: any): value is ServiceDayWorkingHours {
+  return typeof value === "object" && value !== null && "periods" in value;
+}
 export function convertWorkingScheduleToWorkingHours(
-  schedule: WorkingSchedule
+  schedule: WorkingSchedule,
 ): WorkingDate[] {
-  return Object.entries(schedule.weekdays).map(([key, value], i) => ({
-    date: new Date(value?.periods[0] || "").toString(),
-    workingHoursRanges: [
-      {
-        from:
-          typeof value === "object"
-            ? new Date(value?.periods[0] || "").toString()
-            : new Date().toString(),
-        to:
-          typeof value === "object"
-            ? new Date(value?.periods[1] || "").toString()
-            : new Date().toString(),
-      },
-    ],
-  }));
+  const weekdays = schedule.weekdays;
+  const workingDates: WorkingDate[] = [];
+
+  // Iterate over weekdays
+  Object.entries(weekdays).forEach(([key, value]) => {
+    // Check if value is ServiceDayWorkingHours using type guard
+    if (isServiceDayWorkingHours(value)) {
+      // value has the 'periods' property, safe to access value.periods
+      const workingHoursRanges: WorkingHoursRange[] = [];
+
+      value.periods.forEach((period) => {
+        // Parse period and create WorkingHoursRange object
+        const [startTimeStr, endTimeStr] = period.split("-");
+        if (startTimeStr && endTimeStr) {
+          const startTime: string = new Date(startTimeStr.trim()).toString();
+          const endTime: string = new Date(endTimeStr.trim()).toString();
+
+          workingHoursRanges.push({ from: startTime, to: endTime });
+        }
+      });
+
+      // Push to workingDates array
+      workingDates.push({
+        date: key as DateType, // Assuming key is the date or day abbreviation
+        workingHoursRanges: workingHoursRanges,
+      });
+    }
+  });
+
+  return workingDates;
 }
