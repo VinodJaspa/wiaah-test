1234ccf090b1af4b2b2bffeae9f7401c958860c3
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-29 18:14:08 +0300

    refactor: Added placeholder

--- packages/ui/components/sections/ServiceManagement/TimeManagement/WeekdaysSchedule.tsx
+++ packages/ui/components/sections/ServiceManagement/TimeManagement/WeekdaysSchedule.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { WeekdaysOpenTimeInput } from "@UI";
+import { ServiceDayWorkingHours, WeekdaysWorkingHours } from "@features/API";
+// Placeholder data for ServiceDayWorkingHours
+const placeholderServiceDayWorkingHours: ServiceDayWorkingHours = {
+  __typename: "ServiceServiceDayWorkingHours",
+  periods: ["09:00-12:00", "13:00-17:00"],
+};
 
+// Placeholder data for WeekdaysWorkingHours
+const placeholderWeekdaysWorkingHours: WeekdaysWorkingHours = {
+  __typename: "WeekdaysWorkingHours",
+  mo: placeholderServiceDayWorkingHours,
+  tu: placeholderServiceDayWorkingHours,
+  we: placeholderServiceDayWorkingHours,
+  th: placeholderServiceDayWorkingHours,
+  fr: placeholderServiceDayWorkingHours,
+  sa: placeholderServiceDayWorkingHours,
+  su: placeholderServiceDayWorkingHours,
+};
 export const WeekdaysSchedule: React.FC = () => {
   const { t } = useTranslation();
   return (
@@ -9,14 +26,14 @@ export const WeekdaysSchedule: React.FC = () => {
       <p className="text-lg font-semibold text-primary-500 px-4 py-2 bg-primary-50">
         {t(
           "configure_your_time",
-          "Configure your week settings here, must move the sliders and add/remove breaktimes to set up working hours."
+          "Configure your week settings here, must move the sliders and add/remove breaktimes to set up working hours.",
         )}
       </p>
       <WeekdaysOpenTimeInput
         onChange={(data) => {
           console.log("time change", data);
         }}
-        value={[]}
+        value={placeholderWeekdaysWorkingHours}
       />
     </div>
   );
