0f9a5d83bb53a05112552025c26fa5951dae2318
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-30 16:00:18 +0300

    fix: Fixed TQueryKey type

--- packages/ui/src/Hooks/ApiHooks/utils/QueryOptions/index.ts
+++ packages/ui/src/Hooks/ApiHooks/utils/QueryOptions/index.ts
@@ -1,10 +1,10 @@
-import { UseQueryOptions } from "react-query";
+import { QueryKey, UseQueryOptions } from "react-query";
 
 export type queryOptions<
   TQueryFnData = any,
   TError = any,
   TData = any,
-  TQueryKey = any
+  TQueryKey extends QueryKey = QueryKey,
 > = Omit<
   UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
   "queryKey" | "queryFn"
