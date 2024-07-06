ed5db4b405255e46a56eff07260bac35fec1292b
SaifAlqady51 <saifalqady51@gmail.com>
2024-07-06 17:22:16 +0300

    refactor: Added ValueOfFun type

--- packages/utils/src/passPropsToFnOrElem.ts
+++ packages/utils/src/passPropsToFnOrElem.ts
@@ -1,10 +1,14 @@
 import React from "react";
 import { PassPropsToChild } from "utils";
 
+type ValueOrFn<Props> = React.ReactNode | ((props: Props) => React.ReactNode);
+
 export function PassPropsToFnOrElem<Props>(
-  valueOrFn: React.ReactNode,
+  valueOrFn: ValueOrFn<Props>,
   props: Props
 ) {
   const isFn = typeof valueOrFn === "function";
-  return isFn ? valueOrFn(props) : PassPropsToChild(valueOrFn, props);
+  return isFn
+    ? (valueOrFn as (props: Props) => React.ReactNode)(props)
+    : PassPropsToChild(valueOrFn, props);
 }
