37479c86c14a20a8b87ecf0dde84a997cfe47955
SaifAlqady51 <saifalqady51@gmail.com>
2024-07-06 17:21:33 +0300

    fix: Fix runIfFn function output type

--- packages/utils/src/runIfFun.ts
+++ packages/utils/src/runIfFun.ts
@@ -2,11 +2,12 @@ import React from "react";
 
 export type MaybeFn<T> = React.ReactNode | ((props: T) => React.ReactNode);
 
-export function runIfFn<TProps, TReturn = React.ReactNode>(
+export function runIfFn<TProps>(
   valueOrFn: MaybeFn<TProps>,
   props: TProps = {} as TProps
-): TReturn {
+): React.ReactNode {
   const isFn = typeof valueOrFn === "function";
-
-  return isFn ? valueOrFn(props) : valueOrFn;
+  return isFn
+    ? (valueOrFn as (props: TProps) => React.ReactNode)(props)
+    : valueOrFn;
 }
