18d27e4dcd2b20db07454dd1d299da62f0acb3b6
SaifAlqady51 <saifalqady51@gmail.com>
2024-07-11 09:38:37 +0300

    refactor: Refactor QueryBase file

--- apps/back-end/packages/utils/src/CQRS/QueryBase.ts
+++ apps/back-end/packages/utils/src/CQRS/QueryBase.ts
@@ -1,14 +1,16 @@
 import { UserPreferedLang } from "../decorators";
 import { GqlSelectedFields } from "../pipes";
 
+// Define a type for the arguments including the required properties
+type QueryBaseArgs<TInput, TFields> = TInput & {
+  langId: UserPreferedLang;
+  selectedFields: TFields;
+};
+
+// Define the QueryBase class with generic types
 export class QueryBase<
   TInput extends Object,
   TFields extends GqlSelectedFields<any, any>
 > {
-  constructor(
-    public readonly args: TInput & {
-      langId: UserPreferedLang;
-      selectedFields: TFields;
-    }
-  ) {}
+  constructor(public readonly args: QueryBaseArgs<TInput, TFields>) { }
 }
