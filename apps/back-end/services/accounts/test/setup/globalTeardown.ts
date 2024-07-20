f0519cd4ca38678d95b1532acc1cbae022e1d786
SaifAlqady51 <saifalqady51@gmail.com>
2024-07-20 12:56:10 +0300

    refactor: Edited accounts package, tsconfig & tests

--- apps/back-end/services/accounts/test/setup/globalTeardown.ts
+++ apps/back-end/services/accounts/test/setup/globalTeardown.ts
@@ -3,7 +3,6 @@ import { config } from '../config/index';
 
 export = async function globalTeardown() {
   if (config.Memory) {
-    // Config to decided if an mongodb-memory-server instance should be used
     const instance: MongoMemoryServer = (global as any).__MONGOINSTANCE;
     await instance.stop();
   }
