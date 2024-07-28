5755c19d03da93afcfe008fded96706ea7e8f29d
SaifAlqady51 <saifalqady51@gmail.com>
2024-07-28 07:09:53 +0300

    refactor: Returned event-scheduling app.module.ts file to back logic

--- apps/back-end/services/event-scheduling/src/app.module.ts
+++ apps/back-end/services/event-scheduling/src/app.module.ts
@@ -1,26 +1,7 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { PrismaService } from '@prisma-service';
 import { EventSchedulingModule } from './event-scheduling/event-scheduling.module';
-import { PrismaModule } from './prisma.module';
-
-import {
-  ApolloFederationDriver,
-  ApolloFederationDriverConfig,
-} from '@nestjs/apollo';
-import { getUserFromRequest } from 'nest-utils';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-  imports: [
-    EventSchedulingModule,
-    PrismaModule,
-    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
-      driver: ApolloFederationDriver,
-      context: (ctx) => ({ ...ctx, user: getUserFromRequest(ctx.req) }),
-      autoSchemaFile: true,
-    }),
-  ],
+  imports: [EventSchedulingModule],
 })
 export class AppModule { }
