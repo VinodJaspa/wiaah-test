0a8082529a65a2be22698973ea29aa580e9417f8
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-01 16:32:02 +0300

    style: Edited DiscoverIcon styles

--- packages/ui/components/partials/icons/DiscoverIcon.tsx
+++ packages/ui/components/partials/icons/DiscoverIcon.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { HtmlSvgProps } from "types";
+import { HtmlDivProps, HtmlSvgProps } from "types";
+import { FaCompass, FaRegCompass } from "react-icons/fa6";
 
 export const DiscoverOldIcon: React.FC<HtmlSvgProps> = (props) => {
   return (
@@ -18,28 +19,34 @@ export const DiscoverOldIcon: React.FC<HtmlSvgProps> = (props) => {
 export const DiscoverIcon: React.FC<HtmlSvgProps> = (props) => {
   return (
     <svg
-      {...props}
       width="1em"
       height="1em"
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
-      <path
-        d="M10 19.1429C15.0495 19.1429 19.1429 15.0495 19.1429 10C19.1429 4.95054 15.0495 0.857147 10 0.857147C4.95057 0.857147 0.857178 4.95054 0.857178 10C0.857178 15.0495 4.95057 19.1429 10 19.1429Z"
-        fill="currentColor"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        strokeLinejoin="evenodd"
-        clipRule="evenodd"
-        d="M12.2858 11.1429L7.71436 14.5714V8.85716L12.2858 5.42859V11.1429Z"
+      <defs>
+        <mask id="mask0" x="0" y="0" width="20" height="20">
+          <rect x="0" y="0" width="20" height="20" fill="white" />
+          <path
+            d="M12.2858 11.1428L7.71436 14.5714V8.8571L12.2858 5.42853V11.1428Z"
+            stroke="black"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </mask>
+      </defs>
+      <circle
+        cx="10"
+        cy="10"
+        r="9.14286"
         fill="white"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
+        mask="url(#mask0)"
       />
     </svg>
   );
@@ -61,12 +68,11 @@ export const DiscoverOutlineIcon: React.FC<HtmlSvgProps> = (props) => {
         strokeLinejoin="round"
       />
       <path
-        strokeLinejoin="evenodd"
+        strokeLinejoin="inherit"
         clipRule="evenodd"
         d="M12.2858 11.1428L7.71436 14.5714V8.8571L12.2858 5.42853V11.1428Z"
         stroke="currentColor"
         strokeLinecap="round"
-        strokeLinejoin="round"
       />
     </svg>
   );
