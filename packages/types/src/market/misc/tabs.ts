d0fd4fe1a4d79e797423b9c5b61f9e0b46ae0197
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-03 17:21:13 +0300

    refactor: Added optional props to TabTypes

--- packages/types/src/market/misc/tabs.ts
+++ packages/types/src/market/misc/tabs.ts
@@ -2,6 +2,8 @@ import { TranslationTextType } from "types";
 export interface TabType<TComponent = React.ReactElement> {
   name: React.ReactNode | React.FC;
   component: React.ReactNode | React.FC;
+  outlineIcon?: React.ReactNode;
+  solidIcon?: React.ReactNode;
 }
 
 export type TabsTabType = {
