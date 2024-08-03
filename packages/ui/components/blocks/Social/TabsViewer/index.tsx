019178362773b92f12b4ab618b2b1a62a1c3c547
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-03 17:22:23 +0300

    refactor: Edited TabsViewer

--- packages/ui/components/blocks/Social/TabsViewer/index.tsx
+++ packages/ui/components/blocks/Social/TabsViewer/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Tabs,
   TabList,
@@ -30,36 +31,49 @@ export const TabsViewer: React.FC<TabsViewerProps> = ({
   if (!tabs || tabs.length === 0) {
     return <div>Loading...</div>; // Display loading state or message
   }
-  return tabs.length > 0 ? (
+  return (
     <Tabs {...props}>
-      {showTabs && (
-        <TabsHeader className="justify-center flex-wrap py-[0px]">
-          {tabs.map(({ name }, i) => (
-            <TabTitle key={i} TabKey={i}>
-              {({ currentTabIdx }) => {
-                return (
+      {({ currentTabIdx, setCurrentTabIdx }) => (
+        <>
+          {showTabs && (
+            <TabsHeader className="justify-center flex-wrap py-[0px] border-t-2 gap-8">
+              {tabs.map(({ name, outlineIcon, solidIcon }, i) => (
+                <TabTitle key={i} TabKey={i}>
                   <div
-                    className={`${currentTabIdx === i
-                        ? "border-primary text-primary fill-primary font-bold"
-                        : "font-semibold text-lightBlack border-transparent"
-                      } border-b-2 pb-2 leading-6 text-sm`}
+                    onClick={() => setCurrentTabIdx(i)}
+                    className={`flex items-center gap-2 space-x-2  font-semibold text-xs leading-none ${currentTabIdx === i
+                        ? "border-t-2 py-2.5 border-black text-black fill-black "
+                        : "border-none py-3 text-[#8E8E8E]"
+                      }`}
                   >
-                    {runIfFn(name, { active: currentTabIdx === i })}
+                    <>
+                      {outlineIcon && solidIcon && (
+                        <div
+                          className={`mb-[3px] ${currentTabIdx === i
+                              ? "text-black"
+                              : "text-[#8E8E8E]"
+                            }`}
+                        >
+                          {currentTabIdx === i ? solidIcon : outlineIcon}
+                        </div>
+                      )}
+                      {runIfFn(name, { active: currentTabIdx === i })}
+                    </>
                   </div>
-                );
-              }}
-            </TabTitle>
-          ))}
-        </TabsHeader>
-      )}
-      {children}
-      {showPanels && (
-        <TabList className="">
-          {tabs.map(({ component }, i) => (
-            <TabItem key={i}>{runIfFn(component)}</TabItem>
-          ))}
-        </TabList>
+                </TabTitle>
+              ))}
+            </TabsHeader>
+          )}
+          {children}
+          {showPanels && (
+            <TabList className="">
+              {tabs.map(({ component }, i) => (
+                <TabItem key={i}>{runIfFn(component)}</TabItem>
+              ))}
+            </TabList>
+          )}
+        </>
       )}
     </Tabs>
-  ) : null;
+  );
 };
