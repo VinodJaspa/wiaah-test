4429005794dc98b18ebd9fe702795c624b1b240b
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-11 17:19:19 +0300

    feat: Added props to PostViewPopup component

--- packages/ui/components/blocks/Popups/PostViewPopup/index.tsx
+++ packages/ui/components/blocks/Popups/PostViewPopup/index.tsx
@@ -8,20 +8,32 @@ import {
   ArrowDownIcon,
   CommentInput,
   useCommentOnContent,
+  PostCommentCardProps,
+  Divider,
 } from "@UI";
 import { useTranslation } from "react-i18next";
 import { PostCommentCard, Slider } from "@UI";
 import { getParamFromAsPath } from "utils";
-import { PostCardPlaceHolder } from "placeholder";
+import { getRandomImage, PostCardPlaceHolder } from "placeholder";
 import { useActionComments } from "@src/Hooks";
 import { useRouting } from "routing";
-import { useModalDisclouser } from "hooks";
+import { AttachmentType, ContentHostType } from "@features/API";
+import { GoLink } from "react-icons/go";
+
+import { BiBookmark } from "react-icons/bi";
+import { BookingLinkBanner } from "@blocks/BookingLinkBanner";
+
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 
 export interface PostViewPopupProps<TData> {
   renderChild: (props: TData) => React.ReactElement;
   idParam?: string;
   queryName: string;
-  fetcher: (queryFn: any) => Promise<TData>;
+  fetcher: (queryFn: any) => Promise<TData | null>;
+  isOpen: boolean;
+  handleOpen: () => void;
+  handleClose: () => void;
+  showLink?: boolean;
 }
 
 export const goNextPost = async ({ currentId }: { currentId: string }) => {
@@ -37,12 +49,15 @@ export function PostViewPopup<TData extends {}>({
   idParam = "postId",
   queryName = "post",
   fetcher,
+  isOpen,
+  handleOpen,
+  handleClose,
+  showLink = false,
 }: PostViewPopupProps<TData>) {
   const { visit, getCurrentPath } = useRouting();
   const { CloseComments, OpenComments, ToggleComments, open } =
     useActionComments();
   const { t } = useTranslation();
-  const { isOpen, handleClose, handleOpen } = useModalDisclouser();
 
   React.useEffect(() => {
     OpenComments();
@@ -105,72 +120,143 @@ export function PostViewPopup<TData extends {}>({
   return (
     <>
       <Modal onOpen={() => { }} onClose={handlePostViewClose} isOpen={isOpen}>
-        {/* <ModalOverlay className="bg-opacity-[100%]" /> */}
-        <ModalContent className="h-screen px-[2.5rem] bg-black flex w-screen">
-          <div className="flex flex-col gap-2 w-full">
+        <ModalContent className=" h-screen w-screen bg-opacity-15 bg-black flex justify-center items-center ">
+          <div className="flex justify-center items-center relative w-3/4 h-5/6 ">
             <MdClose
               onClick={handlePostViewClose}
-              className={`text-3xl w-8  cursor-pointer text-white`}
+              className={`absolute -top-2 -right-16 text-3xl w-9 h-9 cursor-pointer text-white`}
               aria-label="Close Post"
             />
-            <div className="flex w-full h-full bg-black">
-              <Slider
-                variant="vertical"
-                data-testid="VerticalCarouselContainer"
-              >
-                {renderChild && post && renderChild(post)}
-              </Slider>
+            <div className="flex w-1/2 h-full bg-black">
+              {renderChild(post!)}
             </div>
-          </div>
-          <div className="flex flex-col w-10 text-white justify-between">
-            <ArrowUpIcon
-              onClick={handlePrevPost}
-              className="text-4xl"
-              aria-label="prev Post"
-            />
-            <ArrowDownIcon
-              className="text-4xl"
-              onClick={handleNextPost}
-              aria-label="next Post"
-            />
-          </div>
-          <div
-            style={{
-              opacity: open ? 1 : 0,
-              pointerEvents: open ? "all" : "none",
-              width: open ? "min(100%,35rem)" : "0px",
-            }}
-            className={`thinScroll transition-all transform bg-white flex-col p-2 gap-2 flex overflow-x-scroll`}
-          >
-            <div className="h-full overflow-y-scroll">
-              {PostCardPlaceHolder.postInfo.comments && (
-                <>
-                  {PostCardPlaceHolder.postInfo.comments.length > 0 ? (
-                    PostCardPlaceHolder.postInfo.comments.map(
-                      (comment: any, i: any) => (
-                        <PostCommentCard key={i} {...comment} />
-                      )
-                    )
-                  ) : (
-                    <div className="flex justify-center items-center h-full">
-                      <p className="text-xl font-bold">
-                        {t("no comments in this post")}
-                      </p>
-                    </div>
-                  )}
-                </>
-              )}
-            </div>
-            <CommentInput
-              onCommentSubmit={(v) => {
-                // mutate({
-                //   authorProfileId:
-                // })
+            <div
+              style={{
+                opacity: open ? 1 : 0,
+                pointerEvents: open ? "all" : "none",
+                width: open ? "50%" : "0px",
               }}
-            />
+              className={`transition-all transform bg-white flex-col p-2 pr-3 gap-2 flex h-full `}
+            >
+              {/*Top section (Link)*/}
+              <BookingLinkBanner
+                showLink={showLink}
+                link="https://www.figma.com/design/zou6Q"
+              />
+              <div className="hide-scrollbar h-full overflow-y-scroll">
+                {PostCardPlaceHolder.postInfo.comments && (
+                  <>
+                    {FAKE_COMMENTS.length > 0 ? (
+                      FAKE_COMMENTS.map(
+                        (comment: PostCommentCardProps["comment"], i: any) => (
+                          <>
+                            <PostCommentCard
+                              main={true}
+                              key={i}
+                              comment={comment}
+                            />
+
+                            <Divider className="my-4" />
+                          </>
+                        )
+                      )
+                    ) : (
+                      <div className="flex justify-center items-center h-full">
+                        <p className="text-xl font-bold">
+                          {t("no comments in this post")}
+                        </p>
+                      </div>
+                    )}
+                  </>
+                )}
+              </div>
+              <CommentInput
+                onCommentSubmit={(v) => {
+                  // mutate({
+                  //   authorProfileId:
+                  // })
+                }}
+              />
+            </div>
+            <div className="absolute bottom-0 -right-[68px] z-50  cursor-pointer select-none rounded-full  p-3 bg-white text-white ">
+              <MdOutlineArrowForwardIos className="rotate-90 text-[#20ECA7] w-[22px] h-[22px]" />
+            </div>
           </div>
         </ModalContent>
       </Modal>
     </>
   );
 }
+
+const FAKE_COMMENTS: PostCommentCardProps["comment"][] = [
+  {
+    __typename: "Comment",
+    id: "cmt-123456",
+    content:
+      "This is the first sample comment, This is the first sample comment,This is the first sample comment, This is the first sample comment, This is the first sample comment, This is the first sample comment,This is the first sample comment, This is the first sample comment , This is the first sample comment,This is the first sample comment, This is the first sample comment",
+    commentedAt: "2024-08-10T12:34:56Z",
+    likes: 42,
+    userId: "user-7890",
+    hostId: "host-1234",
+    hostType: ContentHostType.Comment,
+    updatedAt: "2024-08-10T14:00:00Z",
+    replies: 10,
+    author: {
+      __typename: "Profile",
+      username: "johndoe",
+      photo: getRandomImage(),
+      verified: true,
+      id: "profile-5678",
+    },
+  },
+
+  {
+    __typename: "Comment",
+    id: "cmt-789012",
+    content: "This is the second sample comment.",
+    commentedAt: "2024-08-11T10:20:30Z",
+    likes: 28,
+    userId: "user-3456",
+    hostId: "host-5678",
+    hostType: ContentHostType.Comment,
+    updatedAt: "2024-08-11T12:00:00Z",
+    replies: 5,
+    attachment: {
+      __typename: "Attachment",
+      src: getRandomImage(),
+      type: AttachmentType.Img,
+    },
+    author: {
+      __typename: "Profile",
+      username: "janedoe",
+      photo: getRandomImage(),
+      verified: false,
+      id: "profile-9012",
+    },
+  },
+
+  {
+    __typename: "Comment",
+    id: "cmt-345678",
+    content: "This is the third sample comment.",
+    commentedAt: "2024-08-12T15:45:20Z",
+    likes: 15,
+    userId: "user-1122",
+    hostId: "host-3344",
+    hostType: ContentHostType.Comment,
+    updatedAt: "2024-08-12T16:00:00Z",
+    replies: 31,
+    attachment: {
+      __typename: "Attachment",
+      src: getRandomImage(),
+      type: AttachmentType.Img,
+    },
+    author: {
+      __typename: "Profile",
+      username: "alexsmith",
+      photo: getRandomImage(),
+      verified: true,
+      id: "profile-3344",
+    },
+  },
+];
