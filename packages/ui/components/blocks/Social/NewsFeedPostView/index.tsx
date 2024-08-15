c6035ed485be6b98b24f377b843b937145e92463
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-15 15:24:43 +0300

    feat: Created NewsFeedPostView component

---
+++ packages/ui/components/blocks/Social/NewsFeedPostView/index.tsx
@@ -0,0 +1,91 @@
+import { useBreakpointValue, Flex, Button, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import React from "react";
+import { PostCardInfo } from "types";
+import { PostCardPlaceHolder, newsfeedPosts } from "ui/placeholder";
+import { useTranslation } from "react-i18next";
+import { PostCardsListWrapper, PostView, Carousel } from "ui";
+
+export interface NewsFeedPostViewProps {
+  postId: string;
+}
+
+export const NewsFeedPostView: React.FC<NewsFeedPostViewProps> = ({
+  postId,
+}) => {
+  const cols = useBreakpointValue({ base: 1, md: 2, lg: 3 });
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const post = PostCardPlaceHolder;
+  const { data: _post, isLoading: PostIsLoading } = useQuery<PostCardInfo>([
+    "newsFeedPost",
+    { postId: router.query.postId },
+    () => {
+      return PostCardPlaceHolder;
+    },
+  ]);
+
+  const otherPosts = newsfeedPosts;
+  const { data: _otherPosts, isLoading: otherPostsIsLoading } = useQuery<
+    PostCardInfo[]
+  >([
+    "newsFeedPost",
+    { postId: router.query.postId },
+    () => {
+      return newsfeedPosts;
+    },
+  ]);
+
+  return (
+    <Flex pb={{ base: "0.5rem", md: "4rem" }} gap="2rem" direction={"column"}>
+      <div className="flex items-start justify-center h-screen mx-28">
+        <div className="w-full h-5/6 ">
+          <PostView
+            postId="4"
+            queryName="newFeedPost"
+            data={post}
+            idParam="newsfeedpostid"
+            renderChild={(props: PostCardInfo) => {
+              const images = [props.postInfo.thumbnail];
+              return (
+                <Carousel componentsPerView={1} controls={false}>
+                  {images.map((image, index) => (
+                    <div key={index}>
+                      <img src={image} alt={`Attachment ${index + 1}`} />
+                    </div>
+                  ))}
+                </Carousel>
+              );
+            }}
+          />
+        </div>
+      </div>
+      <Text
+        fontSize="xx-large"
+        fontWeight="bold"
+        w="100%"
+        textAlign="center"
+        textTransform="capitalize"
+      >
+        {t("view", "view")} {post.profileInfo.name}{" "}
+        {t("other_posts", "other posts")}
+      </Text>
+      <PostCardsListWrapper cols={cols} posts={otherPosts} />
+      <Button
+        _focus={{ ringColor: "primary.main" }}
+        bgColor="white"
+        borderWidth={"0.25rem"}
+        borderColor="gray"
+        mt="2rem"
+        fontSize={"xl"}
+        color="black"
+        py="0.5rem"
+        textTransform={"capitalize"}
+      >
+        {t("view_more", "view more")}
+      </Button>
+    </Flex>
+  );
+};
