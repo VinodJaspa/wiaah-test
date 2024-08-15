3227f9f17fbfca92eddb5cc96b829603e895bf0b
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-15 15:24:28 +0300

    refactor: Edited ServiceCardListWrapper component

--- packages/ui/components/blocks/Social/ServiceCardListWrapper/index.tsx
+++ packages/ui/components/blocks/Social/ServiceCardListWrapper/index.tsx
@@ -6,7 +6,10 @@ import {
   SocialServicesPostCard,
   SocialServicesPostCardProps,
   useResponsive,
+  PostViewPopup,
+  Carousel,
 } from "@UI";
+import { useModalDisclouser } from "hooks";
 
 export interface ServiceCardsListWrapperProps
   extends Omit<SocialShopCardProps, "shopCardInfo"> {
@@ -23,16 +26,43 @@ export const ServiceCardsListWrapper: React.FC<
   return (
     <>
       <ListWrapper gap={!isMobile} squareItem={false} cols={cols}>
-        {items.map((service, i) => (
-          <SocialServicesPostCard
-            key={i}
-            profileInfo={service.profileInfo}
-            postInfo={service.postInfo}
-            cashback={service.cashback}
-            price={service.price}
-            discount={service.discount}
-          />
-        ))}
+        {items.map((service, i) => {
+          const { isOpen, handleClose, handleOpen } = useModalDisclouser();
+          const images = [service.postInfo.service.thumbnail];
+          return (
+            <React.Fragment key={i}>
+              <SocialServicesPostCard
+                key={i}
+                profileInfo={service.profileInfo}
+                postInfo={service.postInfo}
+                cashback={service.cashback}
+                price={service.price}
+                discount={service.discount}
+                handleOpne={handleOpen}
+              />
+
+              <PostViewPopup
+                isOpen={isOpen}
+                handleOpen={handleOpen}
+                handleClose={handleClose}
+                queryName="shopName"
+                idParam="shopId"
+                data={service}
+                renderChild={(props: SocialServicesPostCardProps) => {
+                  return (
+                    <Carousel componentsPerView={1} controls={true}>
+                      {images.map((image, index) => (
+                        <div key={index}>
+                          <img src={image} alt={`Attachment ${index + 1}`} />
+                        </div>
+                      ))}
+                    </Carousel>
+                  );
+                }}
+              />
+            </React.Fragment>
+          );
+        })}
       </ListWrapper>
     </>
   );
