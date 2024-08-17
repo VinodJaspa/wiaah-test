32e95103e9a57ef928e905a7b91da601ede3bc89
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-17 16:04:05 +0300

    feat: Created [videoId] page

---
+++ apps/front-end/seller/pages/action/post/[videoId].tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NextPage } from "next";
+import Head from "next/head";
+import { SellerLayout, ActionPostView } from "ui";
+import { useRouter } from "next/router";
+
+const ActionPage: NextPage = () => {
+  const router = useRouter();
+  const videoId = router.query.videoId as string;
+  return (
+    <>
+      <Head>Wiaah | Seller Actions</Head>
+      <SellerLayout>
+        <ActionPostView videoId={videoId} />
+      </SellerLayout>
+    </>
+  );
+};
+
+export default ActionPage;
