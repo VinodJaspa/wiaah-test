3b502cea82a7c1d4edcc8b7ca9eb4e583149d955
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-17 16:05:48 +0300

    feat: Created ServicePostView component

---
+++ packages/ui/components/blocks/Social/ServicePostView/index.tsx
@@ -0,0 +1,78 @@
+import { Text } from "@chakra-ui/react";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import {
+  useGetServicePostDetails,
+  PostView,
+  SocialServicePostCardPlaceholder,
+  SocialServicesPostCardProps,
+  Carousel,
+  ServiceCardsListWrapper,
+} from "ui";
+
+export const ServicePostView: React.FC<{ postId: string }> = ({ postId }) => {
+  const { t } = useTranslation();
+  // WARNING: graphql query is not ready yet
+  const {
+    data: _data,
+    isLoading: _isloading,
+    isError: _isError,
+  } = useGetServicePostDetails(postId);
+  const post = SocialServicePostCardPlaceholder.filter(
+    (post) => post.postInfo.id === postId
+  );
+
+  return (
+    <div className="w-full h-full ">
+      {post[0] ? (
+        <div className="flex items-start justify-center h-screen mx-28">
+          <div className="w-full h-5/6 ">
+            <PostView
+              postId="4"
+              queryName="newFeedPost"
+              data={post[0]}
+              idParam="newsfeedpostid"
+              renderChild={(props: SocialServicesPostCardProps) => {
+                const images = [post[0].postInfo.service.thumbnail];
+                return (
+                  <Carousel componentsPerView={1} controls={images.length > 1}>
+                    {images.map((image, index) => (
+                      <div key={index}>
+                        <img src={image} alt={`Attachment ${index + 1}`} />
+                      </div>
+                    ))}
+                  </Carousel>
+                );
+              }}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="w-full flex justify-center items-center">
+          <h1 className="text-xl font-semibold">
+            Post {postId} does not exist
+          </h1>
+        </div>
+      )}
+
+      <Text
+        fontSize={"xx-large"}
+        fontWeight="bold"
+        w="100%"
+        textAlign={"center"}
+        textTransform={"capitalize"}
+      >
+        {t("other_posts", "other posts")}
+      </Text>
+      <div className="flex justify-center w-full h-fit">
+        <div className="md:w-8/12 w-11/12">
+          <ServiceCardsListWrapper
+            cols={3}
+            items={SocialServicePostCardPlaceholder}
+            popup={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
