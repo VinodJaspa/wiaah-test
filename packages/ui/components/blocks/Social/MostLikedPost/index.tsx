d6d6c990ec323057c9c7d0518bbff103e882a34c
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-20 16:41:00 +0300

    ui: Edited MostLikedPost component styles

--- packages/ui/components/blocks/Social/MostLikedPost/index.tsx
+++ packages/ui/components/blocks/Social/MostLikedPost/index.tsx
@@ -6,15 +6,15 @@ interface MostLikedPostProps {
 
 export const MostLikedPost: React.FC<MostLikedPostProps> = ({ image }) => {
   return (
-    <div className="flex flex-col shadow-lg rounded-lg h-[390px] w-[272px]">
-      <div className="relative">
-        <img src={image} className="w-[272px] h-[300px] rounded-t-lg" />
+    <div className="flex flex-col shadow-lg rounded-lg md:h-[390px] h-[187px] md:w-[272px] w-[160px] ">
+      <div className="relative w-full h-3/4 ">
+        <img src={image} className=" rounded-t-lg w-full h-full" />
         <div className="absolute left-2 top-2  font-medium rounded-md bg-black bg-opacity-40 p-2">
           <p className="text-white text-xs">Most Liked Post</p>
         </div>
       </div>
-      <div className="flex justify-center w-[272px] items-center h-[90px] ">
-        <button className=" text-white font-semibold bg-[#3CD399] w-[252px] h-[50px] flex justify-center items-center text-lg rounded-lg ">
+      <div className="flex justify-center w-full items-center h-1/4  px-2.5 ">
+        <button className=" text-white font-semibold bg-[#3CD399] md:w-[252px] w-[150px]  h-1/2 flex justify-center items-center md:text-lg text-sm md:rounded-lg rounded py-2 ">
           View Post
         </button>
       </div>
