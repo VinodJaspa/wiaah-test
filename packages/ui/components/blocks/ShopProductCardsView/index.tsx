403de5c977bc2047cd7ef53a4f3992c72e664379
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-22 13:30:24 +0300

    feat:created ShpProductCardsView component

---
+++ packages/ui/components/blocks/ShopProductCardsView/index.tsx
@@ -0,0 +1,32 @@
+import { ShopProductCard, ShopProductCardProps } from "@blocks/ShopProductCard";
+import { ListWrapper } from "@blocks/Wrappers";
+import { useResponsive } from "@UI/../hooks";
+import React from "react";
+
+interface ShopProductCardsViewProps {
+  products: ShopProductCardProps[];
+}
+
+export const ShopProductCardsView: React.FC<ShopProductCardsViewProps> = ({
+  products,
+}) => {
+  const { isMobile } = useResponsive();
+  return (
+    <div className=" flex justify-center items-center ">
+      <ListWrapper
+        cols={isMobile ? 2 : 4}
+        listProps={{
+          className: "gap-2 md:gap-4 flex flex-col",
+        }}
+        props={{
+          className:
+            "flex justify-between w-fit md:w-full h-full gap-2 md:gap-4 my-6 md:mt-0 ",
+        }}
+      >
+        {products.map((product, i) => (
+          <ShopProductCard key={i} {...product} />
+        ))}
+      </ListWrapper>
+    </div>
+  );
+};
