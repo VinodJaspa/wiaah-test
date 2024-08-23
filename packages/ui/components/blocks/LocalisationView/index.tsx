d09789cd91c7c6a7dca4f728795c33e698a2b755
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-23 15:40:25 +0300

    refactor: Changed LocalisationsView global by putting it in package components

--- packages/ui/components/blocks/LocalisationView/index.tsx
+++ packages/ui/components/blocks/LocalisationView/index.tsx
@@ -1,15 +1,10 @@
 import {
   useBreakpointValue,
-  Flex,
-  HStack,
   Icon,
   Center,
   Spinner,
   Divider,
-  Button,
   Image,
-  Text,
-  VStack,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
@@ -18,7 +13,6 @@ import { MdPlace } from "react-icons/md";
 import { useQuery } from "react-query";
 import { placesPH } from "ui/placeholder";
 import { PlaceCardProps, ListWrapper, PlaceCard } from "ui";
-import { getRandomImage } from "placeholder";
 export interface LocailisationsViewProps { }
 
 export const LocalisationsView: React.FC<LocailisationsViewProps> = ({ }) => {
@@ -27,7 +21,15 @@ export const LocalisationsView: React.FC<LocailisationsViewProps> = ({ }) => {
   const cols = useBreakpointValue({ base: 1, md: 2, lg: 3 });
 
   const router = useRouter();
-  const { tag } = router.query;
+  const tag = router.query.tag as string;
+  const formatCityName = (city: string): string => {
+    return city
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+  };
+
+  const formattedCity = formatCityName(tag);
 
   const { data: places, isLoading } = useQuery<PlaceCardProps[]>(
     "localisations",
@@ -37,43 +39,32 @@ export const LocalisationsView: React.FC<LocailisationsViewProps> = ({ }) => {
   );
 
   return (
-    <Flex direction={"column"} my="2rem">
-      <HStack px="1rem" justify={"space-between"} w="100%">
-        {!isMobile && (
-          <Button visibility={"hidden"} textTransform={"capitalize"}>
-            {t("follow", "follow")}
-          </Button>
-        )}
-        <HStack w="100%">
+    <div className="flex flex-col w-10/12 md:mt-0 mt-6">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-1 items-center">
           <Image
             alt="thumbnail"
             rounded="xl"
-            h="3rem"
+            h="5rem"
             w="auto"
             objectFit="cover"
-            src={getRandomImage()}
+            src="/place-1.jpg"
           />
-          <Flex
-            justify={"center"}
-            w="100%"
-            align={"center"}
-            gap="0.5em"
-            fontWeight={"bold"}
-            fontSize={{ base: "lg", sm: "x-large" }}
-            lineHeight={"1.3em"}
-          >
+          <div className="flex justify-center items-center gap-2 font-bold text-lg">
             <Icon fontSize={"1.5em"} as={MdPlace} />
-            <Flex direction={"column"} align="center">
-              <Text>{tag}</Text>
-              <Text fontWeight={"bold"} fontSize={"normal"}>
-                5.5m
-              </Text>
-            </Flex>
-          </Flex>
-        </HStack>
-        <Button textTransform={"capitalize"}>{t("follow", "follow")}</Button>
-      </HStack>
-      <Divider borderColor={"black"} my="1rem" />
+            <div className="flex items-center gap-1">
+              <p className="text-black font-semibold">
+                {formattedCity ? formattedCity : tag}
+              </p>
+              <p className="text-[#999999] ">(5.5m views) </p>
+            </div>
+          </div>
+        </div>
+        <button className="bg-[#3CD399] text-white px-[54px] py-[14px] font-semibold rounded-full">
+          {t("follow", "follow")}
+        </button>
+      </div>
+      <Divider borderColor={"#E4E4E4"} my="1rem" />
       {isLoading ? (
         <Center>
           <Spinner colorScheme={"primary"} />
@@ -89,6 +80,6 @@ export const LocalisationsView: React.FC<LocailisationsViewProps> = ({ }) => {
             ))}
         </ListWrapper>
       )}
-    </Flex>
+    </div>
   );
 };
