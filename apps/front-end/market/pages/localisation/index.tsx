552d3ca4f24a7e8128d3643ad4098e032b13fb40
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-23 15:42:42 +0300

    refactor: get LocalisationsView from packages

--- apps/front-end/market/pages/localisation/index.tsx
+++ apps/front-end/market/pages/localisation/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import { Container } from "ui";
-import {MasterLayout} from "@components";
-import { LocalisationView } from "@components";
-interface LocalisationProps {}
+import { Container, LocalisationsView } from "ui";
+import { MasterLayout } from "@components";
+interface LocalisationProps { }
 
-export const getServerSideProps: GetServerSideProps<LocalisationProps> =
-  async () => {
-    // prefetch localisations and pass it to react query as
-    // hyderated props
+export const getServerSideProps: GetServerSideProps<
+  LocalisationProps
+> = async () => {
+  // prefetch localisations and pass it to react query as
+  // hyderated props
 
-    return {
-      props: {},
-    };
+  return {
+    props: {},
   };
+};
 
 const localisation: NextPage<LocalisationProps> = () => {
   return (
@@ -24,7 +24,7 @@ const localisation: NextPage<LocalisationProps> = () => {
       </Head>
       <MasterLayout social>
         <Container>
-          <LocalisationView />
+          <LocalisationsView />
         </Container>
       </MasterLayout>
     </>
