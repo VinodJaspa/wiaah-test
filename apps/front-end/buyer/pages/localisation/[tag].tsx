2a958c23509ef6cd4b3faed588576745210fd93c
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-23 15:29:51 +0300

    feat: Created [tag].tsx file inside localisation page

---
+++ apps/front-end/buyer/pages/localisation/[tag].tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { dehydrate, QueryClient } from "react-query";
+import { placesPH } from "ui/placeholder";
+import Head from "next/head";
+import { SellerLayout, useResponsive } from "ui";
+import { LocalisationsView } from "ui";
+
+const getLoclisations = async () => {
+  return placesPH;
+};
+
+interface LocalisationPageProps { }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {}, // You can add your props here
+  };
+};
+
+const Localisation: NextPage = () => {
+  const { isMobile } = useResponsive();
+  return (
+    <>
+      <Head>
+        <title>Buyer | Localisation</title>
+      </Head>
+      <SellerLayout header={isMobile ? "minimal" : "main"}>
+        <LocalisationsView />
+      </SellerLayout>
+    </>
+  );
+};
+
+export default Localisation;
