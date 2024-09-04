01ba36cd1211bd6ceca778b85dcb7926f13f342d
SaifAlqady51 <saifalqady51@gmail.com>
2024-09-04 08:18:39 +0300

    feat: enable cors to auth

--- apps/back-end/services/auth/src/main.ts
+++ apps/back-end/services/auth/src/main.ts
@@ -5,6 +5,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Specify the client's origin
+    credentials: true, // Allow credentials such as cookies
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+    ],
+    exposedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+    ],
+  });
   app.connectMicroservice<MicroserviceOptions>({
     strategy: new KafkaCustomTransport({
       client: {
