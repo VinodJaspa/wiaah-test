c3686531ca94d28a7e015b35b392ddba3f4de249
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-13 13:55:12 +0300

    fix: Fixed CarouselPreviwer test

--- packages/ui/components/blocks/Carousel/Carousel.test.tsx
+++ packages/ui/components/blocks/Carousel/Carousel.test.tsx
@@ -2,7 +2,8 @@ import { shallow } from "enzyme";
 import { Carousel } from "../";
 
 describe("Carousel", () => {
-  it("should  render properly", () => {
-    shallow(<Carousel />);
+  it("should render properly", () => {
+    const children = [<div key="1">Item 1</div>, <div key="2">Item 2</div>]; // Mock children
+    shallow(<Carousel children={children}></Carousel>); // Pass the mock children
   });
 });
