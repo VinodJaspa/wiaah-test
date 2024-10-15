539892490dc3ab43b58a78352829286d274339a8
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-15 15:31:14 +0300

    refactor: Edited SellerCard test

--- packages/ui/components/blocks/SellerCard/SellerCard.test.tsx
+++ packages/ui/components/blocks/SellerCard/SellerCard.test.tsx
@@ -3,6 +3,8 @@ import { shallow } from "enzyme";
 import { SellerCard } from "../SellerCard";
 
 it("renders correctly", () => {
-  const wrapper = shallow(<SellerCard />);
+  const wrapper = shallow(
+    <SellerCard id="3" name="card" rating={5} reviews={5} />,
+  );
   expect(wrapper).toMatchSnapshot();
 });
