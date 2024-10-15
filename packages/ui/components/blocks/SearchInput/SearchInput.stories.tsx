894289a5553f3e260512e37c200ac3ea88446022
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-15 15:31:01 +0300

    refactor: Edited SearchInput storybook component

--- packages/ui/components/blocks/SearchInput/SearchInput.stories.tsx
+++ packages/ui/components/blocks/SearchInput/SearchInput.stories.tsx
@@ -4,37 +4,25 @@ import { SearchInput } from "..";
 import { Country } from "country-state-city";
 import { FlagIcon } from "react-flag-kit";
 import { Prefix } from "../../partials";
+
 export default {
   title: "UI/blocks/SearchInput",
   component: SearchInput,
 } as ComponentMeta<typeof SearchInput>;
 
 const Template: ComponentStory<typeof SearchInput> = (args) => (
-  <SearchInput
-    {...args}
-    components={Country.getAllCountries().map((country, i) => ({
-      name: country.name,
-      comp: (
-        <Prefix prefix={<FlagIcon code={country.isoCode} />}>
-          {country.name}
-        </Prefix>
-      ),
-    }))}
-  />
+  <SearchInput {...args} />
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  explictWidth: {
-    value: 30,
-  },
   placeholder: "search here",
 };
 
 Default.decorators = [
   (Story, { args }) => (
     <section className="flex h-screen w-full flex-col items-center justify-center bg-slate-200">
-      <Story args={args} />
+      <Story {...args} />
     </section>
   ),
 ];
