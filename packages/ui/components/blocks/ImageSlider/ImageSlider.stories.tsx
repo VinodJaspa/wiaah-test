7cc105df9cc9c5da36211b219f4577ce2b4de7f3
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-15 15:19:35 +0300

    refactor: Edited ImageSlider storybook

--- packages/ui/components/blocks/ImageSlider/ImageSlider.stories.tsx
+++ packages/ui/components/blocks/ImageSlider/ImageSlider.stories.tsx
@@ -1,6 +1,7 @@
 import { storybookBlocksTitle } from "utils";
 import { ImageSlider } from "@UI";
 import { ComponentMeta } from "@storybook/react";
+import { imagesPlaceholder } from "@UI/../placeholder";
 
 export default {
   title: storybookBlocksTitle + "ImageSlider",
@@ -8,5 +9,5 @@ export default {
 } as ComponentMeta<typeof ImageSlider>;
 
 export const Default = () => {
-  return <ImageSlider />;
+  return <ImageSlider images={imagesPlaceholder} />;
 };
