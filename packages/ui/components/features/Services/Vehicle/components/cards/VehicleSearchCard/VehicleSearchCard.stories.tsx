6ed4b81564f8dd5fa1b9b2b5be3cb85f6ad4f84c
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-16 12:05:16 +0300

    fix: Fixed VehicleSearchCard storyBook type issue

--- packages/ui/components/features/Services/Vehicle/components/cards/VehicleSearchCard/VehicleSearchCard.stories.tsx
+++ packages/ui/components/features/Services/Vehicle/components/cards/VehicleSearchCard/VehicleSearchCard.stories.tsx
@@ -2,6 +2,7 @@ import { storybookVehicleCardsTitle } from "utils";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { VehicleSearchCard, VehicleSearchCardProps } from "./VehicleSearchCard";
 import React from "react";
+import { ServicePresentationType } from "@features/API/gql/generated";
 
 export default {
   title: storybookVehicleCardsTitle + "VehicleSearchCard",
@@ -16,31 +17,33 @@ const template: ComponentStory<typeof VehicleSearchCard> = (args) => (
 
 export const Default: { args: VehicleSearchCardProps } = template.bind({});
 Default.args = {
-  name: "Lucky Dip Car",
-  pricePerDay: 111,
-  thumbnail:
-    "https://www.autocar.co.uk/sites/autocar.co.uk/files/images/car-reviews/first-drives/legacy/1_rangerover_tracking.jpg",
-
-  vehicleProps: [
+  __typename: "Vehicle",
+  id: "vehicle-1",
+  title: "Sedan 2024",
+  presentations: [
     {
-      type: "a/c",
-      value: true,
-    },
-    {
-      type: "gps",
-      value: true,
-    },
-    {
-      type: "passengers",
-      value: 5,
-    },
-    {
-      type: "windows",
-      value: 4,
-    },
-    {
-      type: "bags",
-      value: 3,
+      __typename: "ServicePresentation",
+      type: ServicePresentationType.Img, // Replace with a valid ServicePresentationType value
+      src: "https://example.com/image.jpg", // Replace with a valid image URL
     },
   ],
+  brand: "Toyota",
+  model: "Camry",
+  price: 30000,
+  cancelationPolicies: [
+    {
+      __typename: "ServiceCancelationPolicy",
+      duration: 14, // Duration in days
+      cost: 100, // Cost of cancellation
+    },
+  ],
+  properties: {
+    __typename: "VehicleProperties",
+    seats: 5,
+    windows: 6,
+    maxSpeedInKm: 240,
+    lugaggeCapacity: 450,
+    gpsAvailable: true,
+    airCondition: true,
+  },
 };
