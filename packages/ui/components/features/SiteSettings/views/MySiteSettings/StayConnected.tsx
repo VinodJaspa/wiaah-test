71ec9b04c0854dd2d1a0f1c19fc76b952992f098
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-17 15:02:13 +0300

    fix: Fixed StayConnected component

--- packages/ui/components/features/SiteSettings/views/MySiteSettings/StayConnected.tsx
+++ packages/ui/components/features/SiteSettings/views/MySiteSettings/StayConnected.tsx
@@ -1,12 +1,12 @@
 import { useTranslation } from "react-i18next";
 
 import React from "react";
-import { useAdminGetSiteSettings } from "@features/Admin";
+import { useAdminGetSiteSettings } from "@features/Admin/services/Queries/useGetAdminSiteSettingsQuery";
 
 const StayConnectedSection: React.FC = () => {
   const { t } = useTranslation();
 
-  const {} = useAdminGetSiteSettings;
+  const { } = useAdminGetSiteSettings;
 
   return <></>;
 };
