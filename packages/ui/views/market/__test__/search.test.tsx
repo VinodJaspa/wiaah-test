3e30cb29eecbbed478d5571128aabe08715acf5f
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-19 16:04:56 +0300

    fix: Fixed search test type issue

--- packages/ui/views/market/__test__/search.test.tsx
+++ packages/ui/views/market/__test__/search.test.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import Search from "../pages/search";
 import { shallow } from "enzyme";
+import { InputSearch } from "@partials";
 it("renders correctly", () => {
-  const wrapper = shallow(<Search />);
+  const wrapper = shallow(
+    <InputSearch options={[]} onOptionSelect={() => { }} />,
+  );
   expect(wrapper).toMatchSnapshot();
 });
