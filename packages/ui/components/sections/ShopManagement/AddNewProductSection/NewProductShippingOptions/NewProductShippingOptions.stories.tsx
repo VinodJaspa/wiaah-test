fa56a800b09440829522095f8ad9c49aae0c6077
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-20 13:42:02 +0300

    refactor: Editee NewProductShippingOptions storyBook

--- packages/ui/components/sections/ShopManagement/AddNewProductSection/NewProductShippingOptions/NewProductShippingOptions.stories.tsx
+++ packages/ui/components/sections/ShopManagement/AddNewProductSection/NewProductShippingOptions/NewProductShippingOptions.stories.tsx
@@ -1,9 +1,19 @@
 import { storybookSectionsTitle, NewProductShippingOptions } from "@UI";
 import { ComponentMeta } from "@storybook/react";
+import * as Yup from "yup";
+
+const shippingValidationSchema = Yup.object().shape({
+  shippingMethods: Yup.array().of(Yup.string()).min(1, "select_at_least_one"),
+});
 
 export default {
   title: storybookSectionsTitle + "NewProductShippingOption",
   component: NewProductShippingOptions,
 } as ComponentMeta<typeof NewProductShippingOptions>;
 
-export const Default = () => <NewProductShippingOptions />;
+export const Default = () => (
+  <NewProductShippingOptions
+    onValid={() => { }}
+    validationSchema={shippingValidationSchema}
+  />
+);
