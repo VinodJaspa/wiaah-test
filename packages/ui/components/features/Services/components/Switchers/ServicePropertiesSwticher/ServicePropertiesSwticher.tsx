f005055d0d8d7c42014fc35cda7f33a09e2ecb6b
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-23 15:51:10 +0300

    refactor: Edited ServicepropertiesSwitcher component

--- packages/ui/components/features/Services/components/Switchers/ServicePropertiesSwticher/ServicePropertiesSwticher.tsx
+++ packages/ui/components/features/Services/components/Switchers/ServicePropertiesSwticher/ServicePropertiesSwticher.tsx
@@ -22,54 +22,42 @@ import {
   SnowFlakeIcon,
   BalconyIcon,
 } from "@UI";
-import { runIfFn } from "utils";
 
-// Define a common interface for all icon components
-interface CommonIconProps {
-  style?: React.CSSProperties;
-}
+export const ServicePropertiesSwticher: React.FC<{ slug: string }> = ({
+  slug,
+}) => {
+  const ServiceProperties = {
+    "a/c": SnowFlakeIcon,
+    gps: GPSIcon,
+    passengers: TransportGuestsIcon,
+    windows: CarWindowIcon,
+    bags: TransportLuggageIcon,
+    pool: PoolIcon,
+    restaurant: SpoonCrossKnifeIcon,
+    parking: ParkingIcon,
+    business_services: BusinessServiceIcon,
+    room_service: DiningBellIcon,
+    "pet-friendly": PetPawIcon,
+    breakfast: CupIcon,
+    laundry: LaundryIcon,
+    wifi: WifiIcon,
+    gym: DumbbellIcon,
+    bar: BarGlassIcon,
+    "24/7_front_desk": DeskIcon,
+    kitchen: ForkAndKnifeIcon,
+    balcony: BalconyIcon,
+    tv: TelevisionIcon,
+  };
 
-type IconComponent = React.ComponentType<CommonIconProps>;
+  const Slug = slug as keyof typeof ServiceProperties;
+  const IconComponent = ServiceProperties[Slug];
 
-export const ServiceProperties: Record<string, IconComponent> = {
-  "a/c": SnowFlakeIcon,
-  gps: GPSIcon,
-  passengers: TransportGuestsIcon,
-  windows: CarWindowIcon,
-  bags: TransportLuggageIcon,
-  pool: PoolIcon,
-  restaurant: SpoonCrossKnifeIcon,
-  parking: ParkingIcon,
-  business_services: BusinessServiceIcon,
-  room_service: DiningBellIcon,
-  "pet-friendly": PetPawIcon,
-  breakfast: CupIcon,
-  laundry: LaundryIcon,
-  wifi: WifiIcon,
-  gym: DumbbellIcon,
-  bar: BarGlassIcon,
-  "24/7_front_desk": DeskIcon,
-  kitchen: ForkAndKnifeIcon,
-  balcony: BalconyIcon,
-  tv: TelevisionIcon,
-};
-
-export interface ServicePropertiesSwitcherProps {
-  slug: string;
-}
-
-export const ServicePropertiesSwticher: React.FC<
-  ServicePropertiesSwitcherProps
-> = ({ slug }) => {
-  const Icon = ServiceProperties[slug] || SuccessIcon;
-
-  return (
-    <Icon
-      style={
-        Icon === SuccessIcon
-          ? { color: "white", fill: "black", backgroundColor: "black" }
-          : undefined
-      }
+  // If the icon exists, render it; otherwise, render the default success icon
+  return IconComponent ? (
+    <IconComponent />
+  ) : (
+    <SuccessIcon
+      style={{ color: "white", fill: "black", backgroundColor: "black" }}
     />
   );
 };
