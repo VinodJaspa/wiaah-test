b75934a23c70feb5e2ce48757f3d037b4976203d
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-23 15:54:11 +0300

    refacotr: Edited useDiscountPriceDisplay

--- packages/ui/components/partials/UnDiscountedPriceDisplay/UnDiscountedPriceDisplay.tsx
+++ packages/ui/components/partials/UnDiscountedPriceDisplay/UnDiscountedPriceDisplay.tsx
@@ -13,16 +13,21 @@ export const UnDiscountedPriceDisplay: React.FC<
   UnDiscountedPriceDisplayProps
 > = ({ amount, discount, decimel, ...props }) => {
   const { currencyCode, currencyRateToUsd, currencySymbol } = useRecoilValue(
-    PreferedCurrencyState
-  );
+    PreferedCurrencyState,
+  ) as {
+    currencyCode: string;
+    currencyRateToUsd: number;
+    currencySymbol: string;
+  }; // Ensure proper type
+
   const convertedPrice = amount * currencyRateToUsd;
-  const unDiscountedPrice = convertedPrice / ((discount - 100) / 100);
+  const unDiscountedPrice = convertedPrice / ((100 - discount) / 100);
+
   return (
     <p
       {...props}
-      className={`${
-        props.className || ""
-      } line-through  decoration-2 whitespace-nowrap`}
+      className={`${props.className || ""
+        } line-through  decoration-2 whitespace-nowrap`}
     >
       {currencySymbol}
       {Math.abs(unDiscountedPrice).toFixed(decimel ? 2 : 0)}
