24b4fee182f85f5b463210a81964792993432719
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-23 15:50:11 +0300

    refactor: Edited GraphqlRequestClient file

--- packages/api/src/utils/GraphqlRequestClient.ts
+++ packages/api/src/utils/GraphqlRequestClient.ts
@@ -1,83 +1,120 @@
-import { gql, GraphQLClient } from "graphql-request";
+import { gql, GraphQLClient, Variables } from "graphql-request";
 import { ReactPubsubClient } from "react-pubsub";
 
-const GraphqlPubsubClient = new ReactPubsubClient();
-
+// Enum for known error codes
 enum KnownErrorCodes {
-  unAuthorized = 0,
-  deniedPremission = 1,
+  Unauthorized = 0,
+  DeniedPermission = 1,
 }
 
-const pubsubEvents = {
-  errorCode: (code: any) => {
-    return `graphql-request-error-${code}`;
-  },
+// Type for GraphQL error
+type GraphQLError = {
+  extensions?: {
+    exception?: {
+      code?: string | number;
+    };
+  };
 };
 
+// PubSub client
+const pubsubClient = new ReactPubsubClient();
+
+// Helper function to create error event name
+const createErrorEventName = (code: string | number) =>
+  `graphql-request-error-${code}`;
+
+// Hook for subscribing to GraphQL request errors
 export const useGraphqlRequestErrorCode = (
   getCode: (codes: typeof KnownErrorCodes) => KnownErrorCodes,
-  cb: () => any
+  callback: () => void,
 ) => {
-  GraphqlPubsubClient.Subscribe(
-    pubsubEvents.errorCode(
-      typeof getCode === "function" ? getCode(KnownErrorCodes) : ""
-    ),
-    cb
-  );
+  const errorCode =
+    typeof getCode === "function" ? getCode(KnownErrorCodes) : "";
+  pubsubClient.Subscribe(createErrorEventName(errorCode), callback);
 };
 
-export class GraphqlRequestClient extends GraphQLClient {
+// GraphqlRequestClient class
+export class GraphqlRequestClient {
+  private client: GraphQLClient;
   private query: string | null = null;
-  private vars: any = {};
+  private variables: Record<string, any> = {};
 
-  setHost(host: string) {
-    this.setEndpoint(host);
+  constructor(
+    url: string,
+    options?: ConstructorParameters<typeof GraphQLClient>[1],
+  ) {
+    this.client = new GraphQLClient(url, options);
+  }
+
+  setHost(host: string): this {
+    this.client.setEndpoint(host);
     return this;
   }
 
-  setQuery(query: string) {
+  setQuery(query: string): this {
     this.query = gql`
       ${query}
     `;
-
     return this;
   }
 
-  setVariables<T>(vars: T) {
-    this.vars = vars;
+  setVariables<T extends Record<string, any>>(variables: T): this {
+    this.variables = variables;
     return this;
   }
 
-  async send<TRes>() {
+  async send<TResponse>(): Promise<{ data: TResponse } | null> {
     try {
-      if (!this.query) throw new Error("no query");
+      if (!this.query) {
+        throw new Error("No query set");
+      }
 
-      const res = await this.rawRequest<TRes>(this.query, this.vars);
-      return res;
-    } catch (error) {
-      // @ts-ignore
-      const errors = error?.response?.errors.map((v) => ({
-        code: v?.extensions?.exception?.code,
-      }));
-
-      const validErrors = errors?.filter(
-        // @ts-ignore
-        (v) => typeof v.code === "string" || typeof v.code === "number"
+      const response = await this.client.rawRequest<TResponse>(
+        this.query,
+        this.variables,
       );
+      return response;
+    } catch (error: unknown) {
+      this.handleError(error);
+      return null;
+    }
+  }
 
-      validErrors?.forEach((v: { code: typeof KnownErrorCodes }) => {
-        const code = pubsubEvents.errorCode(v.code);
-        GraphqlPubsubClient.Publish(code);
-      });
+  async request<T = any, V extends Variables = Variables>(
+    document: string,
+    variables?: V,
+  ): Promise<T> {
+    try {
+      const response = await this.client.request<T>(document, variables);
+      return response;
+    } catch (error: unknown) {
+      this.handleError(error);
+      throw error;
+    }
+  }
 
-      return null as unknown as { data: TRes };
+  private handleError(error: unknown): void {
+    if (error && typeof error === "object" && "response" in error) {
+      const graphqlError = error as { response?: { errors?: GraphQLError[] } };
+      const errors = graphqlError.response?.errors;
+
+      if (errors) {
+        errors.forEach((err) => {
+          const code = err.extensions?.exception?.code;
+          if (code !== undefined) {
+            pubsubClient.Publish(createErrorEventName(code));
+          }
+        });
+      }
     }
   }
 }
 
-export function createGraphqlRequestClient(cred: boolean = true) {
-  const client = new GraphqlRequestClient("http://localhost:3003/graphql", {
-    credentials: cred ? "include" : undefined,
+// Factory function to create GraphqlRequestClient
+export function createGraphqlRequestClient(
+  useCredentials: boolean = true,
+): GraphqlRequestClient {
+  return new GraphqlRequestClient("http://localhost:3003/graphql", {
+    credentials: useCredentials ? "include" : undefined,
   });
-  return client;
 }
