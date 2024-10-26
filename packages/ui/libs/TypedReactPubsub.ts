9dbfc7b6e1b0159bc5913a19a621ad32defce94f
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-26 11:50:35 +0300

    refactor: Updated useGrahqlRequestClient

--- packages/ui/libs/TypedReactPubsub.ts
+++ packages/ui/libs/TypedReactPubsub.ts
@@ -2,5 +2,5 @@ import { ReactPubsubKeysType } from "@const";
 import { useReactPubsub } from "react-pubsub";
 
 export const useTypedReactPubsub = (
-  fn: (keys: ReactPubsubKeysType) => string
+  fn: (keys: ReactPubsubKeysType) => string,
 ) => useReactPubsub<ReactPubsubKeysType>(fn);
--- packages/ui/libs/useGraphqlRequestClient.ts
+++ packages/ui/libs/useGraphqlRequestClient.ts
@@ -1,14 +1,30 @@
-import { GraphqlRequestClient, createGraphqlRequestClient } from "api";
+import { GraphqlRequestClient } from "api";
 import { atom, useRecoilState } from "recoil";
+import { useEffect, useState } from "react";
 
-export const graphqlRequestCtx = atom<GraphqlRequestClient>({
-  default: new GraphqlRequestClient("http://localhost:3003/graphql", {
-    credentials: "include",
-  }),
+// Create a lazy initialization for the GraphQL client to avoid SSR issues.
+export const graphqlRequestCtx = atom<GraphqlRequestClient | null>({
+  default: null,
   key: `graphql-request-client_${Date.now()}`,
 });
 
 export const useGraphqlRequestClient = () => {
-  const [client] = useRecoilState(graphqlRequestCtx);
-  return { client };
+  const [client, setClient] = useRecoilState(graphqlRequestCtx);
+  const [isClientInitialized, setIsClientInitialized] = useState(false);
+
+  useEffect(() => {
+    // Initialize the GraphQL client only on the client side
+    if (!client && typeof window !== "undefined") {
+      const graphqlClient = new GraphqlRequestClient(
+        "http://localhost:3003/graphql",
+        {
+          credentials: "include",
+        },
+      );
+      setClient(graphqlClient);
+      setIsClientInitialized(true);
+    }
+  }, [client, setClient]);
+
+  return { client, isClientInitialized };
 };
