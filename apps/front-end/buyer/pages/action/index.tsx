66ea7c1d432fbe642ff27e754108e954637c95fc
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-30 15:35:51 +0300

    refactor: buyer action index page

--- apps/front-end/buyer/pages/action/index.tsx
+++ apps/front-end/buyer/pages/action/index.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { NextPage } from "next";
 import Head from "next/head";
-import { SellerLayout } from "ui";
-import { ActionsCardListWrapper } from "@blocks/Social/ActionsCardsListWrapper";
-import { SocialActionsCardPlaceholder } from "ui/placeholder/social";
+import { ActionsView, SellerLayout } from "ui";
 
 const ActionsPage: NextPage = () => {
   return (
     <>
-      <Head>Wiaah | Buyer Actions</Head>
+      <Head>Wiaah | Buyer actions</Head>
       <SellerLayout>
-        <div className="flex justify-center w-full h-fit">
-          <div className="md:w-8/12 w-11/12">
-            <ActionsCardListWrapper
-              videos={SocialActionsCardPlaceholder}
-              popup={false}
-            />
-          </div>
-        </div>
+        <ActionsView />
       </SellerLayout>
     </>
   );
