6806e5ae1f4e5cc7b0ba2d4de63a1d27ea6c80a1
SaifAlqady51 <saifalqady51@gmail.com>
2024-10-31 14:52:26 +0300

    refactor: Eidted buyer home page

--- apps/front-end/buyer/pages/index.tsx
+++ apps/front-end/buyer/pages/index.tsx
@@ -1,162 +1,20 @@
 import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useBreakpointValue } from "@chakra-ui/react";
-import {
-  FloatingContainer,
-  PostCardsListWrapper,
-  RecentStories,
-  SellerLayout,
-  SellerPostInput,
-  StoryDisplayProps,
-  AddNewPostModal,
-  AddNewStoryModal,
-  CommentReportModal,
-  VStack,
-  Button,
-} from "ui";
-import { newsfeedPosts } from "ui";
-import { useTranslation } from "react-i18next";
-import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import { useRouter } from "next/router";
-import { socialStoriesPlaceholder } from "placeholder";
+import { SellerLayout } from "ui";
+import SocialNewsfeedView from "@features/Social/views/SocialNewsfeedView";
 
-const RecentStoriesPlaceHolder: StoryDisplayProps[] = [
-  {
-    storyUserData: {
-      id: "1",
-      name: "Wiaah",
-      userPhotoSrc: "/shop-3.jpeg",
-    },
-    seen: true,
-  },
-  {
-    storyUserData: {
-      id: "2",
-      name: "Jack",
-      userPhotoSrc: "/shop.jpeg",
-    },
-    seen: false,
-  },
-  {
-    storyUserData: {
-      id: "3",
-      name: "sam",
-      userPhotoSrc: "/shop-2.jpeg",
-    },
-    seen: true,
-  },
-  {
-    storyUserData: {
-      id: "4",
-      name: "Wiaah",
-      userPhotoSrc: "/shop-3.jpeg",
-    },
-    seen: true,
-  },
-  {
-    storyUserData: {
-      id: "6",
-      name: "Jack",
-      userPhotoSrc: "/shop.jpeg",
-    },
-    seen: false,
-  },
-
-  {
-    storyUserData: {
-      id: "7",
-      name: "Jack",
-      userPhotoSrc: "/shop.jpeg",
-    },
-    seen: false,
-  },
-
-  {
-    storyUserData: {
-      id: "8",
-      name: "Jack",
-      userPhotoSrc: "/shop.jpeg",
-    },
-    seen: false,
-  },
-
-  {
-    storyUserData: {
-      id: "9",
-      name: "Jack",
-      userPhotoSrc: "/shop.jpeg",
-    },
-    seen: false,
-  },
-];
-
-const Buyer: NextPage = () => {
-  const cols = useBreakpointValue({ base: 1, md: 2, lg: 3 });
-  const isMobile = useBreakpointValue({ base: true, md: false });
+const Seller: NextPage = () => {
   return (
     <>
       <Head>
         <title>Wiaah | Buyer</title>
       </Head>
       <SellerLayout header="main">
-        <AddNewPostModal />
-        <AddNewStoryModal />
-        <CommentReportModal />
-        <VStack className="w-full gap-4">
-          <FloatingContainer
-            className="w-full"
-            items={
-              isMobile
-                ? []
-                : [
-                  {
-                    label: (
-                      <Button className="rounded-full bg-white bg-opacity-60 shadow-md">
-                        <ChevronRightIcon boxSize={9} />
-                      </Button>
-                    ),
-                    right: "1rem",
-                    top: "center",
-                  },
-                  {
-                    label: (
-                      <Button className="rounded-full bg-white bg-opacity-60 shadow-md">
-                        <ChevronLeftIcon boxSize={9} />
-                      </Button>
-                    ),
-                    left: "1rem",
-                    top: "center",
-                  },
-                ]
-            }
-          >
-            <RecentStories
-              className="justify-start mx-auto"
-              stories={socialStoriesPlaceholder}
-            />
-          </FloatingContainer>
-          {!isMobile && (
-            <SellerPostInput userName="wiaah" userPhotoSrc="/wiaah_logo.png" />
-          )}
-          <div className="w-full">
-            <PostCardsListWrapper
-              onPostClick={
-                (post) => { }
-                // router.push(
-                //   "/",
-                //   { query: { newsfeedpostid: post.postInfo.id } },
-                //   { shallow: true }
-                // )
-              }
-              cols={cols}
-              posts={newsfeedPosts}
-            />
-          </div>
-        </VStack>
+        <SocialNewsfeedView />
       </SellerLayout>
     </>
   );
 };
 
-export default Buyer;
+export default Seller;
