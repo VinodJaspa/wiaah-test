ba176f85cc46746029a1298bf81503314c73c455
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-01 13:26:36 +0200

    refactor: Updated getShareWithFriends

--- packages/api/src/fetchers/Features/social/share/getShareWithFriends.ts
+++ packages/api/src/fetchers/Features/social/share/getShareWithFriends.ts
@@ -2,38 +2,14 @@ import { ShareWithFriendData } from "types";
 
 export const getShareWithFriends = async (): Promise<ShareWithFriendData[]> => {
   return [
-    {
-      id: "1",
-      name: "wiaah",
-      photo: "/wiaah_logo.png",
-    },
+    { id: "1", name: "wiaah", photo: "/wiaah_logo.png" },
     { id: "2", name: "seller", photo: "/shop.jpeg" },
-    {
-      id: "3",
-      name: "buyer",
-      photo: "/shop-2.jpeg",
-    },
-    {
-      id: "1",
-      name: "wiaah",
-      photo: "/wiaah_logo.png",
-    },
-    { id: "2", name: "seller", photo: "/shop.jpeg" },
-    {
-      id: "3",
-      name: "buyer",
-      photo: "/shop-2.jpeg",
-    },
-    {
-      id: "1",
-      name: "wiaah",
-      photo: "/wiaah_logo.png",
-    },
-    { id: "2", name: "seller", photo: "/shop.jpeg" },
-    {
-      id: "3",
-      name: "buyer",
-      photo: "/shop-2.jpeg",
-    },
+    { id: "3", name: "buyer", photo: "/shop-2.jpeg" },
+    { id: "4", name: "wiaah", photo: "/wiaah_logo.png" },
+    { id: "5", name: "seller", photo: "/shop.jpeg" },
+    { id: "6", name: "buyer", photo: "/shop-2.jpeg" },
+    { id: "7", name: "wiaah", photo: "/wiaah_logo.png" },
+    { id: "8", name: "seller", photo: "/shop.jpeg" },
+    { id: "9", name: "buyer", photo: "/shop-2.jpeg" },
   ];
 };
