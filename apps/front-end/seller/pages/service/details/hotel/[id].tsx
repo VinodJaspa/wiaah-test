5879e6dc05c106f820a736390fa4e0e3073b4c5a
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-07 14:47:21 +0200

    refactor: Edited services/details/hotel page

--- apps/front-end/seller/pages/service/details/hotel/[id].tsx
+++ apps/front-end/seller/pages/service/details/hotel/[id].tsx
@@ -1,16 +1,17 @@
 import { HotelDetailsView } from "@components";
-import { NextPage } from "next";
+import { getHealthCenterDetailsFetcher } from "api";
+import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import React from "react";
+import { dehydrate, QueryClient } from "react-query";
 import { MetaTitle } from "react-seo";
-import { SellerLayout } from "ui";
+import { ServerSideQueryClientProps } from "types";
+import { getHealthCenterDetailsQueryKey, SellerLayout } from "ui";
 
 const ServiceDetails: NextPage = () => {
   return (
     <>
-      <Head>
-        <MetaTitle content="service details" />
-      </Head>
+      <MetaTitle content="service details" />
       <SellerLayout>
         <HotelDetailsView />
       </SellerLayout>
