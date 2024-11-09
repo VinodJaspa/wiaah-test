9881cedbbed290bcb78d222d639792fc06b24190
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-09 15:40:54 +0200

    refactor: Edited ServicesproviderDescriptionSection component

--- packages/ui/components/features/Services/ServicesDetails/components/Sections/ServicesProviderDescriptionSection/ServicesProviderDescriptionSection.tsx
+++ packages/ui/components/features/Services/ServicesDetails/components/Sections/ServicesProviderDescriptionSection/ServicesProviderDescriptionSection.tsx
@@ -1,36 +1,52 @@
+import { Maybe, ServiceAmenity } from "@features/API";
+import { BathTubeIcon, BedIcon, CarIcon, PetPawIcon } from "@partials";
+import { HtmlSvgProps } from "@UI/../types/src";
+import { runIfFn } from "@UI/../utils/src";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { usePublishRef } from "state";
+import { PopularAmenitiesSection } from "../PopularAmenitiesSection";
 export interface ServicesProviderDescriptionSectionProps {
   description: string;
+  bedRooms?: number;
+  bathRooms?: number;
+  petAllowed?: boolean;
+  amenities?: Maybe<
+    Array<
+      { __typename?: "ServiceAmenity" } & Pick<
+        ServiceAmenity,
+        "label" | "value"
+      >
+    >
+  >;
 }
 
 export const ServicesProviderDescriptionSection: React.FC<
   ServicesProviderDescriptionSectionProps
-> = ({ description }) => {
+> = ({ description, amenities, bedRooms, bathRooms, petAllowed }) => {
   const descriptionRef = usePublishRef((keys) => keys.description);
   const { t } = useTranslation();
-  // const items: {
-  //   text: string;
-  //   icon: React.ReactNode;
-  // }[] = [
-  //   {
-  //     text: `${bedrooms} ${t("BedRooms")}`,
-  //     icon: BedIcon,
-  //   },
-  //   {
-  //     text: `${bathrooms} ${t("Bathrooms")}`,
-  //     icon: BathTubeIcon,
-  //   },
-  //   {
-  //     text: `${cars} ${t("Cards")}/${bikes} ${t("Bikes")}`,
-  //     icon: CarIcon,
-  //   },
-  //   {
-  //     text: `${pets} ${t("Pets Allowed")}`,
-  //     icon: PetPawIcon,
-  //   },
-  // ];
+  const items: {
+    text: string;
+    icon: React.ReactNode;
+  }[] = [
+      {
+        text: `${3} ${t("BedRooms")}`,
+        icon: <BedIcon />,
+      },
+      {
+        text: `${1} ${t("Bathrooms")}`,
+        icon: <BathTubeIcon />,
+      },
+      // {
+      //   text: `${4} ${t("Cards")}/${2} ${t("Bikes")}`,
+      //   icon: <CarIcon />,
+      // },
+      {
+        text: ` ${t("Pets Allowed")}`,
+        icon: <PetPawIcon />,
+      },
+    ];
 
   return (
     <div ref={descriptionRef} className="flex flex-col gap-[1.875rem]">
@@ -42,7 +58,7 @@ export const ServicesProviderDescriptionSection: React.FC<
         // }}
         className="grid grid-cols-2 md:grid-cols-4 gap-3"
       >
-        {/* {items.map(({ icon, text }, i) => (
+        {items.map(({ icon, text }, i) => (
           <div
             className="flex rounded-lg bg-[#EFF0F2] text-darkBrown flex-col gap-4 justify-center items-center h-[9.375rem] w-full"
             key={i}
@@ -50,8 +66,12 @@ export const ServicesProviderDescriptionSection: React.FC<
             {runIfFn<HtmlSvgProps>(icon, { className: "text-[2.125rem]" })}
             <p className="font-semibold">{text}</p>
           </div>
-        ))} */}
+        ))}
       </div>
+
+      <PopularAmenitiesSection
+        amenities={amenities.map((amenity) => amenity.label)}
+      />
     </div>
   );
 };
