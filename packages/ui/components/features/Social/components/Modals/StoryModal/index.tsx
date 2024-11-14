d38b728919587d1382a54f813757638fc837a5c0
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-14 14:59:21 +0200

    refactor: Edited StoryModal

--- packages/ui/components/features/Social/components/Modals/StoryModal/index.tsx
+++ packages/ui/components/features/Social/components/Modals/StoryModal/index.tsx
@@ -1,6 +1,16 @@
 import { useTypedReactPubsub } from "@libs";
 import { Modal, StoryViewer, ModalOverlay, ModalContent } from "@UI";
 import React from "react";
+import {
+  EyeIcon,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  PaperPlaneAngleIcon,
+  SmilingFaceFillEmoji,
+} from "@partials";
+import { useTranslation } from "react-i18next";
 
 export const useShowStoryModal = () => {
   const { Listen, emit, removeListner } = useTypedReactPubsub(
@@ -22,6 +32,7 @@ export const useShowStoryModal = () => {
 };
 
 export const ShowStoryModal = () => {
+  const { t } = useTranslation();
   const { Listen, removeListner } = useShowStoryModal();
   const [id, setId] = React.useState<string>();
 
@@ -47,26 +58,67 @@ export const ShowStoryModal = () => {
     <Modal isOpen={!!id} onClose={handleclose} onOpen={() => { }}>
       <ModalOverlay />
       <ModalContent className="w-1/3 h-4/5 p-0">
-        <StoryViewer
-          story={{
-            content:
-              "Lorem Ipsum is simply dummy text of the printing and typesetting",
-            createdAt: new Date().toUTCString(),
-            id: "",
-            src: "/shop.jpeg",
-            type: "img",
-          }}
-          user={{
-            id: "",
-            photo: "/shop.jpeg",
-            username: "name",
-          }}
-          onClose={() => { }}
-          onNext={() => { }}
-          onPrev={() => { }}
-          totalStoryCount={3}
-          currentStory={0}
-        />
+        <div className="h-[93%]">
+          <StoryViewer
+            story={{
+              content:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting",
+              createdAt: new Date().toUTCString(),
+              id: "",
+              src: "/shop.jpeg",
+              type: "img",
+            }}
+            user={{
+              id: "",
+              photo: "/shop.jpeg",
+              username: "name",
+            }}
+            onClose={() => { }}
+            onNext={() => { }}
+            onPrev={() => { }}
+            totalStoryCount={3}
+            currentStory={0}
+          />
+        </div>
+        {false ? (
+          <HStack className=" w-full px-3 py-1 rounded-full self-center border text-white border-white">
+            <EyeIcon className="text-lg" />
+            <p>{t("Views")}</p>
+          </HStack>
+        ) : (
+          <HStack className="w-full self-end gap-6 bg-black">
+            <InputGroup className="w-full rounded-full px-2 pr-4">
+              <Input
+                className="bg-black placeholder:text-white"
+                placeholder={t("Write a message....")}
+              />
+
+              <InputRightElement>
+                <SmilingFaceFillEmoji className="text-white text-xl" />
+              </InputRightElement>
+            </InputGroup>
+            <PaperPlaneAngleIcon
+              className="text-3xl text-white cursor-pointer"
+            // onClick={() => {
+            //   if (typeof value === "string") {
+            //     getUserRoom(
+            //       {
+            //         userId: value,
+            //       },
+            //       {
+            //         onSuccess(data) {
+            //           mutate({
+            //             ...form,
+            //             roomId: data.id,
+            //           });
+            //         },
+            //       },
+            //     );
+            //   }
+            // }}
+            />
+          </HStack>
+        )}
       </ModalContent>
     </Modal>
   );
