5c6f2bd4c23f44cb3d760852e5e57e07c86deb22
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-21 14:37:39 +0200

    refactor: Edited useReactPubsub hook

--- packages/ReactPubsub/src/Hooks/useReactPubsub.ts
+++ packages/ReactPubsub/src/Hooks/useReactPubsub.ts
@@ -2,17 +2,23 @@ import { ReactPubsubContext } from "../ReactProvider";
 import React from "react";
 
 export function useReactPubsub<TKeys>(getKey: (keys: TKeys) => string) {
-  const { publish, subscribe, unSubscribe, keys } =
-    React.useContext(ReactPubsubContext);
+  const context = React.useContext(ReactPubsubContext);
 
-  const key = getKey(keys as TKeys);
+  if (!context) {
+    throw new Error("ReactPubsubContext is not available.");
+  }
 
-  function emit(props?: object) {
+  const { publish, subscribe, unSubscribe, keys } = context;
+
+  const key = React.useMemo(() => getKey(keys as TKeys), [keys, getKey]);
+
+  function emit<TProps = any>(props?: TProps) {
     publish(key, props);
   }
 
   function Listen<TProps = any>(cb: (props?: TProps) => any) {
     subscribe(key, cb);
+    return () => unSubscribe(key); // Return cleanup function
   }
 
   function removeListner() {
