f6b744f1e8afcb6023ef7808909730c531563464
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-22 14:00:12 +0200

    refactor: Edited import paths

--- packages/ui/components/features/Products/services/queries/categories/useGetCategory.ts
+++ packages/ui/components/features/Products/services/queries/categories/useGetCategory.ts
@@ -1,4 +1,4 @@
-import { Exact, ProductCategoryStatus } from "../../../../../features/API";
+import { Exact, ProductCategoryStatus } from "@features/API";
 import { createGraphqlRequestClient } from "api";
 import { useQuery } from "react-query";
 
