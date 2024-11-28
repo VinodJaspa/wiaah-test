3497dc5acfd141c3a11aaf8dce4bba14bce78b85
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-28 14:51:18 +0200

    refacotr: Edited post/affiliation page in market folder

--- apps/front-end/market/pages/social/[profileName]/post/affiliation/[postId].tsx
+++ apps/front-end/market/pages/social/[profileName]/post/affiliation/[postId].tsx
@@ -1,21 +1,30 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import React from "react";
 import { AffiliationOfferCardInfo } from "types";
 import { Container } from "ui";
 import { MasterLayout } from "@components";
 import { AffiliationPostView } from "@components";
-import { useRouting } from "routing";
+import { useRouter } from "next/router";
 
 interface AffiliationPostPageProps {
   affiliationPost: AffiliationOfferCardInfo;
   otherPosts: AffiliationOfferCardInfo[];
 }
 
-const SocialShopPost: NextPage<AffiliationPostPageProps> = ({}) => {
-  const { getParam } = useRouting();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { postId } = context.query;
 
-  const id = getParam("postId");
+  return {
+    props: {
+      postId: postId || null,
+    },
+  };
+};
+
+const SocialShopPost: NextPage<AffiliationPostPageProps> = ({ }) => {
+  const router = useRouter();
+  const postId = router.query.postId as string;
   return (
     <>
       <Head>
@@ -23,7 +32,7 @@ const SocialShopPost: NextPage<AffiliationPostPageProps> = ({}) => {
       </Head>
       <MasterLayout social>
         <Container>
-          <AffiliationPostView id={id} />
+          <AffiliationPostView id={postId} />
         </Container>
       </MasterLayout>
     </>
