0417759a0b1237e9d6f281c49f39b6f513a114cb
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-29 14:10:29 +0200

    refactor: Edited Footer component

--- packages/ui/components/blocks/Footer/index.tsx
+++ packages/ui/components/blocks/Footer/index.tsx
@@ -11,7 +11,7 @@ import {
 export const Footer: React.FC = () => {
   return (
     <>
-      <footer className="block w-full bg-black p-6">
+      <footer className="block w-full bg-slate-800 p-6">
         <div className="container mx-auto">
           <div className="grid w-full grid-cols-1 place-content-center gap-4 md:grid-cols-3 lg:grid-cols-5">
             <SubscribeForm />
