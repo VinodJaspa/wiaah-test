c33ed280c3be902ef316efd02c6dc080a2258665
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-29 14:12:17 +0200

    refactor: Edite social action page

--- apps/front-end/market/pages/social/[profileName]/action/[actionId].tsx
+++ apps/front-end/market/pages/social/[profileName]/action/[actionId].tsx
@@ -3,25 +3,35 @@ import Head from "next/head";
 import React from "react";
 import { MasterLayout } from "@components";
 import { ActionsView } from "@components";
+import { ActionPostView } from "@blocks";
+import { Container } from "@partials";
 
-interface ActionsPageProps {}
+interface ActionsPageProps {
+  actionId: string | null; // Updated to account for null value
+}
 
-export const getServerSideProps: GetServerSideProps<
-  ActionsPageProps
-> = async () => {
+export const getServerSideProps: GetServerSideProps<ActionsPageProps> = async (
+  context,
+) => {
+  const { actionId } = context.query;
+
   return {
-    props: {},
+    props: {
+      actionId: typeof actionId === "string" ? actionId : null, // Ensure type safety
+    },
   };
 };
 
-const actions: NextPage<ActionsPageProps> = () => {
+const actions: NextPage<ActionsPageProps> = ({ actionId }) => {
   return (
     <>
       <Head>
-        <title>Wiaah | actions</title>
+        <title>Wiaah | Actions</title>
       </Head>
       <MasterLayout social>
-        <ActionsView />
+        <Container>
+          <ActionPostView videoId={actionId} />
+        </Container>
       </MasterLayout>
     </>
   );
