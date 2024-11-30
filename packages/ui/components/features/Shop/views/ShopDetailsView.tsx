139aa1c16ff3ef0123cc9fa328c3f614b070fddc
SaifAlqady51 <saifalqady51@gmail.com>
2024-11-30 13:22:44 +0200

    remove: Remove ShopDetialsView from packages folder to market

--- packages/ui/components/features/Shop/views/ShopDetailsView.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ShopDetailsView: React.FC<{ id: string }> = ({ id }) => {
-  return <div></div>;
-};
--- packages/ui/components/features/Shop/views/index.ts
+++ packages/ui/components/features/Shop/views/index.ts
@@ -1,2 +1,1 @@
 export * from "./ShopsSearchView";
-export * from "./ShopDetailsView";
