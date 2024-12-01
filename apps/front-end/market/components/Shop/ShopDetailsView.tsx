cda70d693aecbfed662f3b8a4e116c1a3db65815
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-01 14:32:33 +0200

    refactor: Edited ShopDetilasView component

--- apps/front-end/market/components/Shop/ShopDetailsView.tsx
+++ apps/front-end/market/components/Shop/ShopDetailsView.tsx
@@ -1,13 +1,12 @@
-import { SearchView, ShopProductCardsView } from "@UI";
-import { shopProductCardsPlaceholder } from "placeholder";
+import { SearchView } from "@UI";
 import React from "react";
 import { ShopProfile } from "./ShopProfile";
 
 export const ShopDetailsView: React.FC<{ id: string }> = ({ id }) => {
   return (
-    <div className="flex flex-col w-full ">
+    <div className="flex flex-col w-full h-full ">
       <ShopProfile shopId={id} />
-      <div className="flex justify-between w-full ">
+      <div className="flex justify-between w-full h-full ">
         <SearchView />
       </div>
     </div>
