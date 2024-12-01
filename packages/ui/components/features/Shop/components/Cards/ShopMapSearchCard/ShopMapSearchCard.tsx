649e9f82f6c5cca029eb9fb2e8b0c3287895d916
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-01 14:31:38 +0200

    refacotr: Edited ShopMapSearchCard component

--- packages/ui/components/features/Shop/components/Cards/ShopMapSearchCard/ShopMapSearchCard.tsx
+++ packages/ui/components/features/Shop/components/Cards/ShopMapSearchCard/ShopMapSearchCard.tsx
@@ -1,7 +1,7 @@
 import { ShopMapSearchDataType } from "api";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { AspectRatioImage, Rate } from "@UI";
+import { AspectRatioImage } from "@UI";
 
 export interface ShopMapSearchCardProps extends ShopMapSearchDataType {
   children?: React.ReactNode;
@@ -14,18 +14,31 @@ export const ShopMapSearchCard: React.FC<ShopMapSearchCardProps> = ({
   thumbnail,
   children,
   location,
+  description,
+  reviews,
+  price,
 }) => {
   const { address, city, country, postalCode, state } = location;
   const { t } = useTranslation();
   return (
-    <div className="flex flex-col w-full">
-      <AspectRatioImage src={thumbnail} alt={name} ratio={9 / 16} />
-      <div className="p-2">
-        <p className="font-bold">{name}</p>
+    <div className="flex gap-3 p-2 h-fit border border-2 broder-gray-500 rounded-xl w-full">
+      <img src={thumbnail} className="w-32 h-32 rounded-md" alt={name} />
+      <div className=" flex flex-col gap-2 w-fit">
+        <p className="text-primary underline">{address}</p>
+        <p className="text-lg font-semibold">{name}</p>
+        <p>{description}</p>
+      </div>
+      <div className="flex flex-col gap-2 text-gray-500">
+        <div className="flex gap-1">
+          <p className="text-sm ">{reviews} reviews</p>
+          <p className="bg-primary p-1 text-lg text-white">{rate}</p>
+        </div>
+        <p className="text-primary underline">Show on map</p>
+        <p>{price}$</p>
+        <p>Including taxes & fees</p>
         <p>
-          {address}, {postalCode} {state}, {city}, {country}
+          {country}, {city}, {address}
         </p>
-        <Rate rating={rate} />
       </div>
     </div>
   );
