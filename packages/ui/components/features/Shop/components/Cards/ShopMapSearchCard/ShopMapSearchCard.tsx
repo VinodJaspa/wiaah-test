a2e2092923f0fff198c2a837636547d9b7dcdc74
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-02 15:17:45 +0200

    refactor: Edited ShopMapSearchCard style

--- packages/ui/components/features/Shop/components/Cards/ShopMapSearchCard/ShopMapSearchCard.tsx
+++ packages/ui/components/features/Shop/components/Cards/ShopMapSearchCard/ShopMapSearchCard.tsx
@@ -22,21 +22,23 @@ export const ShopMapSearchCard: React.FC<ShopMapSearchCardProps> = ({
   const { t } = useTranslation();
   return (
     <div className="flex gap-3 p-2 h-fit border border-2 broder-gray-500 rounded-xl w-full">
-      <img src={thumbnail} className="w-32 h-32 rounded-md" alt={name} />
+      <img src={thumbnail} className="w-36 h-36 rounded-md" alt={name} />
       <div className=" flex flex-col gap-2 w-fit">
         <p className="text-primary underline">{address}</p>
         <p className="text-lg font-semibold">{name}</p>
         <p>{description}</p>
       </div>
-      <div className="flex flex-col gap-2 text-gray-500">
-        <div className="flex gap-1">
+      <div className="flex flex-col gap-1 text-gray-500">
+        <div className="flex gap-1 items-center justify-end">
           <p className="text-sm ">{reviews} reviews</p>
-          <p className="bg-primary p-1 text-lg text-white">{rate}</p>
+          <p className="bg-primary p-[2px] text-lg text-white rounded-md">
+            {rate}
+          </p>
         </div>
         <p className="text-primary underline">Show on map</p>
         <p>{price}$</p>
-        <p>Including taxes & fees</p>
-        <p>
+        <p className="whitespace-nowrap">Including taxes & fees</p>
+        <p className="whitespace-nowrap">
           {country}, {city}, {address}
         </p>
       </div>
