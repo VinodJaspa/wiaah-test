c890da31d87bc626b532bf430ae6eb37ed08da56
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-03 14:59:09 +0200

    refactor: Edited ShopOnMapSearchViw compnent

--- apps/front-end/market/components/Shop/ShopOnMapSearchView.tsx
+++ apps/front-end/market/components/Shop/ShopOnMapSearchView.tsx
@@ -8,12 +8,14 @@ import {
   ShopsOnMapSearchList,
 } from "ui";
 
-export const ShopOnMapSearchView: React.FC = () => {
+export const ShopOnMapSearchView: React.FC<{ location: string }> = ({
+  location,
+}) => {
   const { isTablet } = useResponsive();
   return (
     <div className="flex p-4 flex-col gap-2">
       <span className="w-full md:w-1/2">
-        <LocationSearchInput onLocationSelect={() => {}} />
+        <LocationSearchInput onLocationSelect={() => { }} />
       </span>
       <div className="w-full relative pb-40 md:pb-0 flex-col-reverse md:flex-row h-auto md:h-[75vh] flex gap-8 md:gap-4 justify-between">
         <div className="w-full absolute bottom-0 left-0 z-50 md:static md:w-full md:h-full">
@@ -22,7 +24,7 @@ export const ShopOnMapSearchView: React.FC = () => {
           </ScrollingWrapper>
         </div>
         <div className="w-full h-[75vh] md:h-auto">
-          <RenderMap />
+          <RenderMap location={location} />
         </div>
       </div>
     </div>
