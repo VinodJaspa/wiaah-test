bf06e2449867ce2afabfc0227d17ddf84a6be2b4
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-04 15:28:58 +0200

    refactor: Edited Services/utils index.ts file

--- packages/ui/components/features/Services/Services/utils/index.ts
+++ packages/ui/components/features/Services/Services/utils/index.ts
@@ -3,36 +3,46 @@ import { WorkingSchedule } from "../types";
 import { ServiceDayWorkingHours } from "@features/API/gql/generated";
 
 function isServiceDayWorkingHours(value: any): value is ServiceDayWorkingHours {
-  return typeof value === "object" && value !== null && "periods" in value;
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    "periods" in value &&
+    Array.isArray(value.periods) &&
+    value.periods.every((period) => typeof period === "string")
+  );
 }
+
 export function convertWorkingScheduleToWorkingHours(
   schedule: WorkingSchedule,
 ): WorkingDate[] {
   const weekdays = schedule.weekdays;
   const workingDates: WorkingDate[] = [];
 
-  // Iterate over weekdays
-  Object.entries(weekdays).forEach(([key, value]) => {
-    // Check if value is ServiceDayWorkingHours using type guard
+  Object.entries(weekdays).forEach(([dayOrDate, value]) => {
     if (isServiceDayWorkingHours(value)) {
-      // value has the 'periods' property, safe to access value.periods
       const workingHoursRanges: WorkingHoursRange[] = [];
 
       value.periods.forEach((period) => {
-        // Parse period and create WorkingHoursRange object
         const [startTimeStr, endTimeStr] = period.split("-");
-        if (startTimeStr && endTimeStr) {
-          const startTime: string = new Date(startTimeStr.trim()).toString();
-          const endTime: string = new Date(endTimeStr.trim()).toString();
 
-          workingHoursRanges.push({ from: startTime, to: endTime });
+        if (startTimeStr && endTimeStr) {
+          const startTime = startTimeStr.trim();
+          const endTime = endTimeStr.trim();
+
+          if (startTime && endTime) {
+            workingHoursRanges.push({
+              from: startTime,
+              to: endTime,
+            });
+          } else {
+            console.log("Invalid Time Range:", startTime, endTime);
+          }
         }
       });
 
-      // Push to workingDates array
       workingDates.push({
-        date: key as DateType, // Assuming key is the date or day abbreviation
-        workingHoursRanges: workingHoursRanges,
+        date: dayOrDate as DateType,
+        workingHoursRanges,
       });
     }
   });
