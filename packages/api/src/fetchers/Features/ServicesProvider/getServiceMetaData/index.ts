06d98e731a59c0914d0cb0cdd4c0159d62760b68
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-08 14:52:57 +0200

    refactor: Added query files to index.ts file

--- packages/api/src/fetchers/Features/ServicesProvider/getServiceMetaData/index.ts
+++ packages/api/src/fetchers/Features/ServicesProvider/getServiceMetaData/index.ts
@@ -1,1 +1,5 @@
 export * from "./getRestaurantMetadata/";
+export * from "./getVehicleMetadata";
+export * from "./getHotelMetaData/";
+export * from "./getBeautyCenterMetaData/";
+export * from "./getHealthCenterMetadata";
