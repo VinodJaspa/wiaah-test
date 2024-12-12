ed99aec2216ad585d308c8e955d01f7bab34cc80
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-12 15:58:49 +0200

    refactor: Added export to index.ts file

--- packages/ui/components/features/Services/views/index.ts
+++ packages/ui/components/features/Services/views/index.ts
@@ -2,3 +2,4 @@ export * from "./ServiceDetailsView";
 export * from "./MarketServiceSearchResaultsView";
 export * from "./MarketServiceSearchView";
 export * from "./MarketServiceDetailsDrawer";
+export * from "./MarketServiceSearchCardHoverOverlay";
