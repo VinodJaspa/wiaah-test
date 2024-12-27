05424d514c3e0caa589c47219cc0e2e962b9cdff
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-27 18:57:02 +0200

    refactor: Make ImageCard more reusable by extending HTMLDivElement props

--- packages/ui/components/blocks/ImageCard/index.tsx
+++ packages/ui/components/blocks/ImageCard/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-export interface ImageCardProps {
+export interface ImageCardProps extends React.HTMLAttributes<HTMLDivElement> {
   imgUrl?: string;
   name?: string;
 }
 
-export const ImageCard: React.FC<ImageCardProps> = ({ imgUrl, name = "" }) => {
+export const ImageCard: React.FC<ImageCardProps> = ({
+  imgUrl,
+  name = "",
+  ...props
+}) => {
   return (
     <>
-      <div className="w-70 block space-y-3">
+      <div className="w-70 block space-y-3 cursor-pointer" {...props}>
         <img src={imgUrl} alt="img" className="w-70 h-70 object-cover" />
         <div className="flex w-full justify-center">
           <p className="uppercase">{name}</p>
