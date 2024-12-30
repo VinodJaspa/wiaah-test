3f82c610277f8806c2d80abc78a1e10cdb76a3ba
SaifAlqady51 <saifalqady51@gmail.com>
2024-12-30 18:50:21 +0200

    refactor: Edited service-shop/service page

--- apps/front-end/admin/pages/service-shop/services/index.tsx
+++ apps/front-end/admin/pages/service-shop/services/index.tsx
@@ -66,141 +66,51 @@ const Services: NextPage = () => {
                 <Th>{t("Service")}</Th>
                 <Th>{t("Seller")}</Th>
                 <Th>{t("Id")}</Th>
+                <Th>{t("Type of item")}</Th>
                 <Th>{t("Price")}</Th>
-                <Th>{t("Type")}</Th>
-                <Th>{t("Day Clicks")}</Th>
-                <Th>{t("Earnings")}</Th>
-                <Th>{t("Sales")}</Th>
-                <Th>{t("Total Ordered Items")}</Th>
-                <Th>{t("Total Discounted Orders")}</Th>
-                <Th>{t("Total Discounted Amount")}</Th>
-                <Th>{t("Items Refunded")}</Th>
-                <Th>{t("Refund Rate")}</Th>
-                <Th>{t("Positive feedback received")}</Th>
-                <Th>{t("Received Positive feedback rate")}</Th>
-                <Th>{t("Negative feedback received")}</Th>
-                <Th>{t("Received negative feedback rate")}</Th>
-                <Th>{t("Status")}</Th>
-                <Th>{t("Views")}</Th>
                 <Th>{t("Date modified")}</Th>
                 <Th>{t("Action")}</Th>
               </Tr>
-              <Tr>
-                <Th></Th>
-                <Th></Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input type="number" />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Select>
-                    <SelectOption value={"hotel"}>{t("hotel")}</SelectOption>
-                  </Select>
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Select>
-                    <SelectOption value={"active"}>{t("Active")}</SelectOption>
-                    <SelectOption value={"inActive"}>
-                      {t("inActive")}
-                    </SelectOption>
-                  </Select>
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-                <Th>
-                  <Input />
-                </Th>
-              </Tr>
             </THead>
-
             <TBody>
-              <div>
-                {mapArray(services.data, (serv, i) => (
-                  <Tr key={serv.id}>
-                    <Td>
-                      <Checkbox />
-                    </Td>
-                    <Td>
-                      <Image
-                        className="w-full"
-                        src={serv.thumbnail}
-                        alt="thumbnail"
+              {services.data.map((serv, i) => (
+                <Tr data-testid="service-record" key={serv.id}>
+                  <Td>
+                    <Checkbox />
+                  </Td>
+                  <Td className="min-w-[8rem]">
+                    <Image
+                      className="w-full"
+                      src={serv.thumbnail}
+                      alt="thumbnail"
+                    />
+                  </Td>
+                  <Td>{serv.title}</Td>
+                  <Td>{serv.shop.sellerProfile.username}</Td>
+                  <Td>{serv.id.slice(0, 8)}...</Td>
+                  <Td>
+                    <PriceDisplay price={serv.price} />
+                  </Td>
+                  <Td>{new Date(serv.updatedAt).toDateString()}</Td>
+                  <Td>
+                    <div className="grid grid-cols-2d justify-center gap-2 fill-white text-white text-sm ">
+                      <SearchIcon className="w-8 h-8 p-2 bg-cyan-600" />
+                      <EditIcon
+                        onClick={() =>
+                          visit((r) =>
+                            r
+                              .addPath(getCurrentPath())
+                              .addPath("edit")
+                              .addPath(serv.id),
+                          )
+                        }
+                        className="w-8 h-8 p-2 bg-cyan-400"
                       />
-                    </Td>
-                    <Td>{serv.title}</Td>
-                    <Td>{serv.shop.sellerProfile.username}</Td>
-                    <Td>{serv.id.slice(0, 8)}...</Td>
-                    <Td>
-                      <PriceDisplay price={serv.price[0]} />
-                    </Td>
-                    <Td className="whitespace-nowrap">{serv.type}</Td>
-                    <Td>{serv.shop.status}</Td>
-                    <Td>{new Date(serv.updatedAt).toDateString()}</Td>
-                    <Td>
-                      <div className="grid grid-cols-2d justify-center gap-2 fill-white text-white text-sm ">
-                        <SearchIcon className="w-8 h-8 p-2 bg-cyan-600" />
-                        <EditIcon
-                          onClick={() =>
-                            visit((r) =>
-                              r
-                                .addPath(getCurrentPath())
-                                .addPath("edit")
-                                .addPath(serv.id)
-                            )
-                          }
-                          className="w-8 h-8 p-2 bg-cyan-400"
-                        />
-                        <TrashIcon
-                          onClick={() => handleDeleteService(serv.id)}
-                          className="w-8 h-8 p-2 bg-red-500"
-                        />
-                      </div>
-                    </Td>
-                  </Tr>
-                ))}
-              </div>
+                      <TrashIcon className="w-8 h-8 p-2 bg-red-500" />
+                    </div>
+                  </Td>
+                </Tr>
+              ))}
             </TBody>
           </Table>
         </TableContainer>
