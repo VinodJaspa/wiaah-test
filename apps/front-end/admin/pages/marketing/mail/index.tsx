11c7cea096835b92448a55aaced141577bfe4bb6
SaifAlqady51 <saifalqady51@gmail.com>
2025-01-03 14:35:49 +0200

    refactor: Edited Marketing page in Admin folder

--- apps/front-end/admin/pages/marketing/mail/index.tsx
+++ apps/front-end/admin/pages/marketing/mail/index.tsx
@@ -19,6 +19,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { setTestid, useForm } from "utils";
 import { MailUserType } from "@features/API";
+import Head from "next/head";
 
 const Mailing: NextPage = () => {
   const { t } = useTranslation();
@@ -29,115 +30,120 @@ const Mailing: NextPage = () => {
   const { mutate: sendMail } = useAdminSendMailToUsers();
 
   return (
-    <section>
-      <div className="flex py-4 justify-end">
-        <Button
-          onClick={() => {
-            sendMail(form);
-          }}
-          {...setTestid("sendBtn")}
-          className="fill-white text-white flex items-center gap-2"
-        >
-          <EmailIcon />
-          <p>{t("Send")}</p>
-        </Button>
-      </div>
-      <div className="border">
-        <div className="text-lg border-b border-b-gray-300 p-4 flex items-center gap-2">
-          <EmailIcon />
-          <p>{t("Send e-mail's to customers")}</p>
-        </div>
-
-        <div className="p-4">
-          <Table
-            TdProps={{ className: "odd:w-1/4 even:w-3/4" }}
-            className="w-full"
+    <React.Fragment>
+      <Head>
+        <title>Admin | Market - Mail</title>
+      </Head>
+      <section>
+        <div className="flex py-4 justify-end">
+          <Button
+            onClick={() => {
+              sendMail(form);
+            }}
+            {...setTestid("sendBtn")}
+            className="fill-white text-white flex items-center gap-2"
           >
-            <TBody>
-              <Tr>
-                <Td>{t("From")}</Td>
-                <Td>
-                  <Select className="w-full">
-                    <SelectOption value={"default"}>
-                      {t("Default")}
-                    </SelectOption>
-                  </Select>
-                </Td>
-              </Tr>
-
-              <Tr>
-                <Td>{t("to")}</Td>
-                <Td>
-                  <Select
-                    {...setTestid("sendToSelect")}
-                    {...selectProps("userType")}
-                    className="w-full"
-                  >
-                    <SelectOption
-                      {...setTestid("sendToOption")}
-                      value={MailUserType.Subscribers}
-                    >
-                      {t("All Newsletter Subscribers")}
-                    </SelectOption>
-                    <SelectOption
-                      {...setTestid("sendToOption")}
-                      value={MailUserType.All}
-                    >
-                      {t("All Customers")}
-                    </SelectOption>
-                    <SelectOption
-                      {...setTestid("sendToOption")}
-                      value={MailUserType.Shops}
-                    >
-                      {t("Products sellers")}
-                    </SelectOption>
-                    <SelectOption
-                      {...setTestid("sendToOption")}
-                      value={MailUserType.Service}
-                    >
-                      {t("Services sellers")}
-                    </SelectOption>
-                    <SelectOption
-                      {...setTestid("sendToOption")}
-                      value={MailUserType.Buyers}
-                    >
-                      {t("Buyers")}
-                    </SelectOption>
-                  </Select>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td>
-                  <InputRequiredStar />
-                  {t("Subject")}
-                </Td>
-                <Td>
-                  <Input
-                    {...inputProps("subject")}
-                    {...setTestid("subject")}
-                    placeholder={t("Subject")}
-                  />
-                </Td>
-              </Tr>
-              <Tr>
-                <Td valign="top">
-                  <InputRequiredStar />
-                  {t("Message")}
-                </Td>
-                <Td>
-                  <Textarea
-                    {...setTestid("message")}
-                    {...inputProps("message")}
-                    placeholder={t("message")}
-                    className="h-72"
-                  />
-                </Td>
-              </Tr>
-            </TBody>
-          </Table>
+            <EmailIcon />
+            <p>{t("Send")}</p>
+          </Button>
         </div>
-      </div>
-    </section>
+        <div className="border">
+          <div className="text-lg border-b border-b-gray-300 p-4 flex items-center gap-2">
+            <EmailIcon />
+            <p>{t("Send e-mail's to customers")}</p>
+          </div>
+
+          <div className="p-4">
+            <Table
+              TdProps={{ className: "odd:w-1/4 even:w-3/4" }}
+              className="w-full"
+            >
+              <TBody>
+                <Tr>
+                  <Td>{t("From")}</Td>
+                  <Td>
+                    <Select className="w-full">
+                      <SelectOption value={"default"}>
+                        {t("Default")}
+                      </SelectOption>
+                    </Select>
+                  </Td>
+                </Tr>
+
+                <Tr>
+                  <Td>{t("to")}</Td>
+                  <Td>
+                    <Select
+                      {...setTestid("sendToSelect")}
+                      {...selectProps("userType")}
+                      className="w-full"
+                    >
+                      <SelectOption
+                        {...setTestid("sendToOption")}
+                        value={MailUserType.Subscribers}
+                      >
+                        {t("All Newsletter Subscribers")}
+                      </SelectOption>
+                      <SelectOption
+                        {...setTestid("sendToOption")}
+                        value={MailUserType.All}
+                      >
+                        {t("All Customers")}
+                      </SelectOption>
+                      <SelectOption
+                        {...setTestid("sendToOption")}
+                        value={MailUserType.Shops}
+                      >
+                        {t("Products sellers")}
+                      </SelectOption>
+                      <SelectOption
+                        {...setTestid("sendToOption")}
+                        value={MailUserType.Service}
+                      >
+                        {t("Services sellers")}
+                      </SelectOption>
+                      <SelectOption
+                        {...setTestid("sendToOption")}
+                        value={MailUserType.Buyers}
+                      >
+                        {t("Buyers")}
+                      </SelectOption>
+                    </Select>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>
+                    <InputRequiredStar />
+                    {t("Subject")}
+                  </Td>
+                  <Td>
+                    <Input
+                      {...inputProps("subject")}
+                      {...setTestid("subject")}
+                      placeholder={t("Subject")}
+                    />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td valign="top">
+                    <InputRequiredStar />
+                    {t("Message")}
+                  </Td>
+                  <Td>
+                    <Textarea
+                      {...setTestid("message")}
+                      {...inputProps("message")}
+                      placeholder={t("message")}
+                      className="h-72"
+                    />
+                  </Td>
+                </Tr>
+              </TBody>
+            </Table>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
   );
 };
 
