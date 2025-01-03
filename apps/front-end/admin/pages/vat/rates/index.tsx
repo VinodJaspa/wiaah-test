8948aa471a3e2b8310749ab14507e3b7f86275d7
SaifAlqady51 <saifalqady51@gmail.com>
2025-01-03 14:34:33 +0200

    refactor: Edited rates page in Admin folder

--- apps/front-end/admin/pages/vat/rates/index.tsx
+++ apps/front-end/admin/pages/vat/rates/index.tsx
@@ -12,6 +12,7 @@ import { mapArray, randomNum, useForm } from "utils";
 import { useTranslation } from "react-i18next";
 import { useRouting } from "routing";
 import { AdminListTable, AdminTableCellTypeEnum } from "@components";
+import Head from "next/head";
 
 const TaxRates: NextPage = () => {
   const { t } = useTranslation();
@@ -25,72 +26,80 @@ const TaxRates: NextPage = () => {
   const data = FAKE_TAXES;
 
   return (
-    <section>
-      <AdminListTable
-        pagination={controls}
-        props={{ TdProps: { className: "border-b border-b-gray-300" } }}
-        onAdd={() => {
-          visit((r) =>
-            r.addPath(getCurrentPath()).addPath("form").addPath("new")
-          );
-        }}
-        headers={[
-          {
-            type: AdminTableCellTypeEnum.checkbox,
-          },
-          {
-            value: t("Tax Name"),
-            inputProps: inputProps("name"),
-          },
-          {
-            value: t("Tax Rate"),
-            inputProps: inputProps("rate"),
-          },
-          {
-            value: t("Geo Zone"),
-          },
-          {
-            value: t("Action"),
-          },
-        ]}
-        title={t("Tax Rate List")}
-        data={mapArray(data, (v) => ({
-          id: v.id,
-          cols: [
+    <React.Fragment>
+      <Head>
+        <title>Admin | Vat Rates</title>
+      </Head>
+      <section>
+        <AdminListTable
+          pagination={controls}
+          props={{ TdProps: { className: "border-b border-b-gray-300" } }}
+          onAdd={() => {
+            visit((r) =>
+              r.addPath(getCurrentPath()).addPath("form").addPath("new"),
+            );
+          }}
+          headers={[
             {
               type: AdminTableCellTypeEnum.checkbox,
             },
             {
-              value: v.title,
+              value: t("Tax Name"),
+              inputProps: inputProps("name"),
             },
             {
-              value: v.percent.toString(),
-              type: AdminTableCellTypeEnum.number,
+              value: t("Tax Rate"),
+              inputProps: inputProps("rate"),
             },
             {
-              value: v.appliedOnCountries.map((v) => v.name).join(", "),
+              value: t("Geo Zone"),
             },
             {
-              type: AdminTableCellTypeEnum.action,
-              actionBtns: [
-                <Button
-                  key={v.id}
-                  onClick={() =>
-                    visit((r) =>
-                      r.addPath(getCurrentPath()).addPath("form").addPath(v.id)
-                    )
-                  }
-                  center
-                  className="w-8 h-8"
-                >
-                  <EditIcon />
-                </Button>,
-              ],
+              value: t("Action"),
             },
-          ],
-        }))}
-      />
-    </section>
+          ]}
+          title={t("Tax Rate List")}
+          data={mapArray(data, (v) => ({
+            id: v.id,
+            cols: [
+              {
+                type: AdminTableCellTypeEnum.checkbox,
+              },
+              {
+                value: v.title,
+              },
+              {
+                value: v.percent.toString(),
+                type: AdminTableCellTypeEnum.number,
+              },
+              {
+                value: v.appliedOnCountries.map((v) => v.name).join(", "),
+              },
+              {
+                type: AdminTableCellTypeEnum.action,
+                actionBtns: [
+                  <Button
+                    key={v.id}
+                    onClick={() =>
+                      visit((r) =>
+                        r
+                          .addPath(getCurrentPath())
+                          .addPath("form")
+                          .addPath(v.id),
+                      )
+                    }
+                    center
+                    className="w-8 h-8"
+                  >
+                    <EditIcon />
+                  </Button>,
+                ],
+              },
+            ],
+          }))}
+        />
+      </section>
+    </React.Fragment>
   );
 };
 
