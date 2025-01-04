297e4536abbc643e7b8aca8a39c10745b6b7bb04
SaifAlqady51 <saifalqady51@gmail.com>
2025-01-04 19:00:14 +0200

    refactor: Edited profession in Admin folder

--- apps/front-end/admin/pages/profession/form/[id].tsx
+++ apps/front-end/admin/pages/profession/form/[id].tsx
@@ -17,6 +17,7 @@ import { useTranslation } from "react-i18next";
 import { useForm, WiaahLangId, WiaahLanguageCountries } from "utils";
 import { useRouting } from "routing";
 import { ProfessionStatus } from "@features/API";
+import Head from "next/head";
 
 const AddProfession: NextPage = () => {
   const { t } = useTranslation();
@@ -54,67 +55,72 @@ const AddProfession: NextPage = () => {
     : updateTranslationInputProps;
 
   return (
-    <section>
-      <AdminListTable
-        onBack={() => back()}
-        onSave={() => (isNew ? create(createForm) : update(updateForm))}
-        data={[]}
-        headers={[]}
-        title={isNew ? t("New") : t("Edit")}
-      >
-        <div className="grid grid-cols-4 gap-4">
-          <HStack>
-            <InputRequiredStar />
-            <p>{t("Profession Name")}</p>
-          </HStack>
-          <InputGroup className="col-span-3">
-            <InputLeftElement>
-              <Select
-                value={lang}
-                onOptionSelect={(v: WiaahLangId) => setLang(v)}
-                className="p-[0px] w-[5rem] border-[0px]"
-              >
-                {WiaahLanguageCountries.map(({ code }) => (
-                  <SelectOption key={code} value={code}>
-                    <FlagIcon code={code} />
-                  </SelectOption>
-                ))}
-              </Select>
-            </InputLeftElement>
+    <React.Fragment>
+      <Head>
+        <title>Admin | Edit Profession Form </title>
+      </Head>
+      <section>
+        <AdminListTable
+          onBack={() => back()}
+          onSave={() => (isNew ? create(createForm) : update(updateForm))}
+          data={[]}
+          headers={[]}
+          title={isNew ? t("New") : t("Edit")}
+        >
+          <div className="grid grid-cols-4 gap-4">
+            <HStack>
+              <InputRequiredStar />
+              <p>{t("Profession Name")}</p>
+            </HStack>
+            <InputGroup className="col-span-3">
+              <InputLeftElement>
+                <Select
+                  value={lang}
+                  onOptionSelect={(v: WiaahLangId) => setLang(v)}
+                  className="p-[0px] w-[5rem] border-[0px]"
+                >
+                  {WiaahLanguageCountries.map(({ code }) => (
+                    <SelectOption key={code} value={code}>
+                      <FlagIcon code={code} />
+                    </SelectOption>
+                  ))}
+                </Select>
+              </InputLeftElement>
+              <Input
+                {...translationInput("title", lang)}
+                placeholder={t("Profession name")}
+              />
+            </InputGroup>
+            <HStack>
+              <InputRequiredStar />
+              <p>{t("Sort order")}</p>
+            </HStack>
             <Input
-              {...translationInput("title", lang)}
-              placeholder={t("Profession name")}
+              {...inputProps("sortOrder")}
+              placeholder={t("Sort Order")}
+              className="col-span-3"
+              type="number"
             />
-          </InputGroup>
-          <HStack>
-            <InputRequiredStar />
-            <p>{t("Sort order")}</p>
-          </HStack>
-          <Input
-            {...inputProps("sortOrder")}
-            placeholder={t("Sort Order")}
-            className="col-span-3"
-            type="number"
-          />
 
-          <HStack>
-            <InputRequiredStar />
-            <p>{t("Status")}</p>
-          </HStack>
-          <Select
-            {...selectProps("status")}
-            placeholder={t("Sort Order")}
-            className="col-span-3"
-          >
-            {Object.values(ProfessionStatus).map((v, i) => (
-              <SelectOption key={i} value={v}>
-                {v}
-              </SelectOption>
-            ))}
-          </Select>
-        </div>
-      </AdminListTable>
-    </section>
+            <HStack>
+              <InputRequiredStar />
+              <p>{t("Status")}</p>
+            </HStack>
+            <Select
+              {...selectProps("status")}
+              placeholder={t("Sort Order")}
+              className="col-span-3"
+            >
+              {Object.values(ProfessionStatus).map((v, i) => (
+                <SelectOption key={i} value={v}>
+                  {v}
+                </SelectOption>
+              ))}
+            </Select>
+          </div>
+        </AdminListTable>
+      </section>
+    </React.Fragment>
   );
 };
 
