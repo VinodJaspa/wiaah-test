f32cbdeb502bf81abe4da6aa23694fb9e10401e7
shohan-pherones <shohan.sub.56@gmail.com>
2025-02-10 22:35:17 +0600

    fix: while clicking the hashtag it will send to the hashtag page

--- packages/ui/components/blocks/DataDisplay/HashtagSearchItem/index.tsx
+++ packages/ui/components/blocks/DataDisplay/HashtagSearchItem/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { HiHashtag } from "react-icons/hi";
 import { NumberShortner } from "../../../helpers";
 import { HtmlDivProps, SearchHashtagItem } from "types";
+import Link from "next/link";
 
 export interface HashTagSearchProps extends SearchHashtagItem {
   props?: HtmlDivProps;
@@ -17,7 +18,9 @@ export const HashTagSearchItem: React.FC<HashTagSearchProps> = ({
     <div className="flex gap-2 items-cneter w-full justify-between" {...props}>
       <div className="flex items-center gap-4">
         <HiHashtag className="border-[1px] border-gray-300 rounded-full p-2 text-5xl" />
-        <span className="font-bold">{hashtagName}</span>
+        <Link href={`/hashtag/${hashtagName}`} className="font-bold">
+          {hashtagName}
+        </Link>
       </div>
       <div className="flex items-center gap-2">
         <span>{NumberShortner(hashtagViews)}</span>
